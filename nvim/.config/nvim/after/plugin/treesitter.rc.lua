7c0316066b7d71c9de9fce991d675413b300cb4d
xavier-castro <xavier_castro@icloud.com>
2022-12-01 21:06:47 -0600

    feat(treesitter): configure rainbow brackets

--- nvim/.config/nvim/after/plugin/treesitter.rc.lua
+++ nvim/.config/nvim/after/plugin/treesitter.rc.lua
@@ -21,7 +21,12 @@ configs.setup {
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
+    colors = {
+      "Gold",
+      "Orchid",
+      "DodgerBlue",
+    }, -- table of hex strings
+    disable = { "html" }, -- list of language that will be disabled
     -- termcolors = {} -- table of colour name strings
   },
   highlight = {
