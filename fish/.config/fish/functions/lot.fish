89fa762b2acb9b27c80d952809622bdcd72fa77d
xavier-castro <xavier_castro@icloud.com>
2022-12-04 22:06:38 -0600

    feat(gitigore): updated fish

---
+++ fish/.config/fish/functions/lot.fish
@@ -0,0 +1,3 @@
+function lot --wraps='nvim ~/.dotfiles/personal/PARKING_LOT.md' --description 'alias lot=nvim ~/.dotfiles/personal/PARKING_LOT.md'
+  nvim ~/.dotfiles/personal/PARKING_LOT.md $argv; 
+end
