c7b6cc1ead80973d458e78589041b3cfeb2481c0
xavier-castro <xavier_castro@icloud.com>
2023-03-17 00:39:26 -0500

    add zen mode

--- nvim/.config/nvim/after/plugin/zen-mode.rc.lua
+++ nvim/.config/nvim/after/plugin/zen-mode.rc.lua
@@ -1,7 +1,29 @@
-local status, zenMode = pcall(require, "zen-mode")
-if (not status) then return end
+vim.keymap.set("n", "<leader>zz", function()
+  require("zen-mode").setup {
+    window = {
+      width = 90,
+      options = {}
+    },
+  }
+  require("zen-mode").toggle()
+  vim.wo.wrap = false
+  vim.wo.number = true
+  vim.wo.rnu = true
+  ColorMyPencils()
+end)
 
-zenMode.setup {
-}
 
-vim.keymap.set('n', '<C-w>o', '<cmd>ZenMode<cr>', { silent = true })
+vim.keymap.set("n", "<leader>zZ", function()
+  require("zen-mode").setup {
+    window = {
+      width = 80,
+      options = {}
+    },
+  }
+  require("zen-mode").toggle()
+  vim.wo.wrap = false
+  vim.wo.number = false
+  vim.wo.rnu = false
+  vim.opt.colorcolumn = "0"
+  ColorMyPencils()
+end)
