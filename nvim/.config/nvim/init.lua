56fe9862db996826118b10851816c90986a7ea82
xavier-castro <xavier_castro@icloud.com>
2023-03-16 23:27:20 -0500

    reset nvim from base

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,22 +1,19 @@
-require "core"
+require('craftzdog.base')
+require('craftzdog.highlights')
+require('craftzdog.maps')
+require('craftzdog.plugins')
 
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+local is_wsl = has "wsl"
 
-if custom_init_path then
-  dofile(custom_init_path)
+if is_mac then
+  require('craftzdog.macos')
 end
-
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
-if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
+if is_win then
+  require('craftzdog.windows')
 end
-
-vim.opt.rtp:prepend(lazypath)
-require "plugins"
-
-dofile(vim.g.base46_cache .. "defaults")
+if is_wsl then
+  require('craftzdog.wsl')
+end
