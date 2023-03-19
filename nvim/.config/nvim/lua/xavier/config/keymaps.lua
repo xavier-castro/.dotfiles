266ba83dba7036d3e6634add7d1427618ef8ac40
xavier-castro <xavier_castro@icloud.com>
2023-03-19 00:31:29 -0500

    setup for merge

--- nvim/.config/nvim/lua/xavier/config/keymaps.lua
+++ nvim/.config/nvim/lua/xavier/config/keymaps.lua
@@ -47,8 +47,6 @@ vim.keymap.set("v", "<leader>d", '"_d')
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 -- vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
- vim.keymap.set("n", "<leader>n", "<cmd>NullFormat!<cr>",
-                                   {buffer = bufnr})
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -56,10 +54,10 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s",
-    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+               [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- remap to open the Telescope refactoring menu in visual mode
 vim.api.nvim_set_keymap("v", "<leader>rr",
-    "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
-    { noremap = true })
+                        "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
+                        {noremap = true})
