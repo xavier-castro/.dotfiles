82bcaa16232b04918ab54a5d2b7d1a95a7238fc2
xavier-castro <xavier_castro@icloud.com>
2023-03-18 22:43:19 -0500

    feat: setup default lsp

--- nvim/.config/nvim/lua/xavier/plugins/lsp.lua
+++ nvim/.config/nvim/lua/xavier/plugins/lsp.lua
@@ -14,18 +14,14 @@ return {
             { 'L3MON4D3/LuaSnip' },     -- Required
         },
         config = function()
-            local lsp = require('lsp-zero').preset("recommended")
+            local lsp = require('lsp-zero').preset({})
 
             lsp.on_attach(function(client, bufnr)
                 lsp.default_keymaps({ buffer = bufnr })
             end)
 
-            lsp.set_sign_icons({
-                error = '✘',
-                warn = '▲',
-                hint = '⚑',
-                info = '»'
-            })
+            -- (Optional) Configure lua language server for neovim
+            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
             local cmp = require('cmp')
             local cmp_action = require('lsp-zero').cmp_action()
