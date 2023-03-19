82aba8f773fc11f26e929323f16ae9c7e618e439
xavier-castro <xavier_castro@icloud.com>
2023-03-19 01:24:23 -0500

    feat: add refactoring keybinds

--- nvim/.config/nvim/lua/xavier/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/xavier/plugins/refactoring.lua
@@ -4,7 +4,14 @@ return {
 		"theprimeagen/refactoring.nvim",
 		config = function()
 			require("refactoring").setup()
-			vim.keymap.set("n", "<leader>rr", vim.cmd.Refactor)
+			vim.keymap.set("n", "<leader>rr", function()
+				vim.api.nvim_set_keymap(
+					"v",
+					"<leader>rr",
+					":lua require('refactoring').select_refactor()<CR>",
+					{ noremap = true, silent = true, expr = false }
+				)
+			end)
 		end,
 	},
 }
