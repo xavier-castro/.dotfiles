7f331dd8badd97aa86eb9a8b3af496b6cef857a2
xavier-castro <xavier_castro@icloud.com>
2023-03-30 09:28:47 -0500

    Refactor key mappings and plugin initialization in nvim config.
    - Improve plugin compatibility by replacing `vim.keymap` with `vim.api.nvim_set_keymap`
    - Refactor and improve code readability by separating plugin initialization onto its own table
    - Add new LSP-related keymappings, `nvim-cmp` and `luasnip` support
    - Update plugin commits for `lsp-zero.nvim`, `lualine.nvim`, `mason.nvim`, `nvim-navbuddy`, and `nvim-treesitter`
    - Delete unnecessary `nvimtree.lua` file from directory.
    
    [nvim/.config/nvim/lua/xavier/plugins/refactoring.lua]
    - Add a table around the return values containing `{}` for clarity
    - Replace `vim.keymap` with `vim.api.nvim_set_keymap` for better compatibility
    - Combine and shorten the arguments for `vim.api.nvim_set_keymap` to avoid redundancy
    - Separate the `theprimeagen/refactoring.nvim` plugin initialization onto its own table for readibility
    [nvim/.config/nvim/lua/xavier/plugins/nvimtree.lua]
    - Delete the `nvimtree.lua` file from `nvim/.config/nvim/lua/xavier/plugins/` directory
    [nvim/.config/nvim/lua/xavier/plugins/lsp.lua]
    - Add a new keymapping for `NullLsInfo`
    - Add several keymapping for LSP related actions (definition, hover, symbol search, etc)
    - Add `nvim-cmp` and `luasnip` support
    - Add code formatting for `nvim-cmp`
    - Add `lspkind.nvim` for symbol icons
    - Remove `"nvim-typescript"` as a dependency for `VonHeikemen/lsp-zero.nvim`
    [nvim/.config/nvim/lazy-lock.json]
    - Update the commit of `lsp-zero.nvim` to `ea4c9511c94df9596c450036502b7ff43d40f816`
    - Update the commit of `lualine.nvim` to `e80465d721bdafcd234e890d1677ca4afb905144`
    - Update the commit of `mason.nvim` to `5e059a03b2273d918e9742d38e5e1cb37c2239f2`
    - Update the commit of `nvim-navbuddy` to `a4620c86c72d8911222fe00191ed390c7f9748c9`
    - Update the commit of `nvim-treesitter` to `09275650b8ae262ac173398976093b60e36e01e8`

--- nvim/.config/nvim/lua/xavier/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/xavier/plugins/refactoring.lua
@@ -1,20 +1,26 @@
 ---@diagnostic disable, 113: 113
-return {{
-    "theprimeagen/refactoring.nvim",
-    config = function()
-        require("refactoring").setup()
-        vim.keymap.set("n", "<leader>rr", function()
-            vim.api.nvim_set_keymap("v", "<leader>rr", ":lua require('refactoring').select_refactor()<CR>", {
-                noremap = true,
-                silent = true,
-                expr = false
-            })
-            vim.api.nvim_set_keymap("v", "<leader>ri",
-                [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {
+return {
+    {
+        "theprimeagen/refactoring.nvim",
+        config = function()
+            require("refactoring").setup()
+            vim.keymap.set("n", "<leader>rr", function()
+                vim.api.nvim_set_keymap("v", "<leader>rr", ":lua require('refactoring').select_refactor()<CR>", {
                     noremap = true,
                     silent = true,
-                    expr = false
+                    expr = false,
                 })
-        end)
-    end
-}}
+                vim.api.nvim_set_keymap(
+                    "v",
+                    "<leader>ri",
+                    [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
+                    {
+                        noremap = true,
+                        silent = true,
+                        expr = false,
+                    }
+                )
+            end)
+        end,
+    },
+}
