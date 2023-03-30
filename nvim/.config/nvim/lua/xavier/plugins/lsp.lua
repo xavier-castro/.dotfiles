b9209f3e09b45566c77ce2fc80f889fc686a4972
xavier-castro <xavier_castro@icloud.com>
2023-03-30 09:59:51 -0500

    Add database session and remove startup-nvim plugin configuration
    - Add and configure new plugins for managing database sessions and navigating files
    - Remove old plugin with its configuration
    - Update package list to reflect changes
    
    [nvim/.config/nvim/lua/xavier/plugins/dbsession.lua]
    - Add a new lua file for managing database sessions
    - Add `dashboard-nvim` plugin with a custom configuration
    - Add keybindings for `dashboard-nvim`
    - Add `dbsession.nvim` plugin with `SessionSave`, `SessionDelete`, and `SessionLoad` commands
    - Add custom configuration to `dbsession.nvim`
    - Add empty metadata lines for each file as per git diff format.
    [nvim/.config/nvim/lua/xavier/plugins/startup.lua]
    - Delete `xavier/plugins/startup.lua` and its content
    - The file contained settings for the `startup-nvim` plugin
    - The configuration called `require("startup").setup()`
    - The plugin had dependencies on `nvim-telescope/telescope.nvim` and `nvim-lua/plenary.nvim`
    [nvim/.config/nvim/lazy-lock.json]
    - Update `lazy-lock.json` with new commits and branches.
    - Add two new packages: `dashboard-nvim` and `dbsession.nvim`.
    - Remove `startup.nvim`.
    [nvim/.config/nvim/lua/xavier/plugins/nav-buddy.lua]
    - Added a plugin to nvim called nvim-navbuddy
    - Made some modifications to `nav-buddy.lua` file
    - Added `vim.keymap.set("n", "<C-b>", "<cmd>Navbuddy<cr>")` to `nav-buddy.lua`
    - Removed `"MunifTanjim/nui.nvim"` from the dependencies list
    - Added `["<esc>"] = actions.close` to the `mappings` list in `nav-buddy.lua`
    - Added a new `config` section to the `navbuddy.setup` function in `nav-buddy.lua`

--- nvim/.config/nvim/lua/xavier/plugins/lsp.lua
+++ nvim/.config/nvim/lua/xavier/plugins/lsp.lua
@@ -1,115 +1,115 @@
 return {
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v2.x",
-        dependencies = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" }, -- Required
-            {
-                -- Optional
-                "williamboman/mason.nvim",
-                build = function()
-                    pcall(vim.cmd, "MasonUpdate")
-                end,
-            },
-            { "williamboman/mason-lspconfig.nvim" }, -- Optional
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{
+				-- Optional
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" }, -- Required
-            { "hrsh7th/cmp-nvim-lsp" }, -- Required
-            { "L3MON4D3/LuaSnip" }, -- Required
-            { "onsails/lspkind.nvim" },
-        },
-        config = function()
-            local lsp = require("lsp-zero").preset({
-                manage_nvim_cmp = {
-                    set_sources = "recommended",
-                    set_basic_mappings = true,
-                    set_extra_mappings = false,
-                    use_luasnip = true,
-                    set_format = true,
-                    documentation_window = true,
-                },
-            })
-            lsp.on_attach(function(client, bufnr)
-                lsp.default_keymaps({
-                    buffer = bufnr,
-                })
-                local opts = { buffer = bufnr, remap = false }
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "onsails/lspkind.nvim" },
+		},
+		config = function()
+			local lsp = require("lsp-zero").preset({
+				manage_nvim_cmp = {
+					set_sources = "recommended",
+					set_basic_mappings = true,
+					set_extra_mappings = false,
+					use_luasnip = true,
+					set_format = true,
+					documentation_window = true,
+				},
+			})
+			lsp.on_attach(function(client, bufnr)
+				lsp.default_keymaps({
+					buffer = bufnr,
+				})
+				local opts = { buffer = bufnr, remap = false }
 
-                vim.keymap.set("n", "<leader>nls", "<cmd>NullLsInfo<cr>", opts)
-                vim.keymap.set("n", "gd", function()
-                    vim.lsp.buf.definition()
-                end, opts)
-                vim.keymap.set("n", "K", function()
-                    vim.lsp.buf.hover()
-                end, opts)
-                vim.keymap.set("n", "<leader>vws", function()
-                    vim.lsp.buf.workspace_symbol()
-                end, opts)
-                vim.keymap.set("n", "<leader>vd", function()
-                    vim.diagnostic.open_float()
-                end, opts)
-                vim.keymap.set("n", "[d", function()
-                    vim.diagnostic.goto_next()
-                end, opts)
-                vim.keymap.set("n", "]d", function()
-                    vim.diagnostic.goto_prev()
-                end, opts)
-                vim.keymap.set("n", "<leader>vca", function()
-                    vim.lsp.buf.code_action()
-                end, opts)
-                vim.keymap.set("n", "<leader>vrr", function()
-                    vim.lsp.buf.references()
-                end, opts)
-                vim.keymap.set("n", "<leader>vrn", function()
-                    vim.lsp.buf.rename()
-                end, opts)
-                vim.keymap.set("i", "<C-h>", function()
-                    vim.lsp.buf.signature_help()
-                end, opts)
-                vim.keymap.set("n", "<M-.>", function()
-                    vim.lsp.buf.code_action()
-                end, opts)
-                vim.keymap.set("i", "<M-.>", function()
-                    vim.lsp.buf.code_action()
-                end, opts)
-            end)
+				vim.keymap.set("n", "<leader>nls", "<cmd>NullLsInfo<cr>", opts)
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
+				vim.keymap.set("n", "<leader>vws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+				vim.keymap.set("n", "<leader>vca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrr", function()
+					vim.lsp.buf.references()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrn", function()
+					vim.lsp.buf.rename()
+				end, opts)
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+				vim.keymap.set("n", "<M-.>", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				vim.keymap.set("i", "<M-.>", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+			end)
 
-            -- (Optional) Configure lua language server for neovim
-            require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-            lsp.setup()
+			-- (Optional) Configure lua language server for neovim
+			require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+			lsp.setup()
 
-            -- (Optional) Configure nvim-cmp
-            local cmp = require("cmp")
-            local cmp_action = require("lsp-zero").cmp_action()
-            require("luasnip.loaders.from_vscode").lazy_load()
-            cmp.setup({
-                sources = {
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                },
-                mapping = {
-                    ["<M-f>"] = cmp_action.luasnip_jump_forward(),
-                    ["<M-b>"] = cmp_action.luasnip_jump_backward(),
-                    ["<Tab>"] = cmp_action.luasnip_supertab(),
-                    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.close(),
-                    ["<CR>"] = cmp.mapping.confirm({
-                        behavior = cmp.ConfirmBehavior.Replace,
-                        select = true,
-                    }),
-                },
-                formatting = {
-                    fields = { "abbr", "kind", "menu" },
-                    format = require("lspkind").cmp_format({
-                        mode = "symbol", -- show only symbol annotations
-                        maxwidth = 50, -- prevent the popup from showing more than provided characters
-                        ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-                    }),
-                },
-            })
-        end,
-    },
+			-- (Optional) Configure nvim-cmp
+			local cmp = require("cmp")
+			local cmp_action = require("lsp-zero").cmp_action()
+			require("luasnip.loaders.from_vscode").lazy_load()
+			cmp.setup({
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				},
+				mapping = {
+					["<M-f>"] = cmp_action.luasnip_jump_forward(),
+					["<M-b>"] = cmp_action.luasnip_jump_backward(),
+					["<Tab>"] = cmp_action.luasnip_supertab(),
+					["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.close(),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+				},
+				formatting = {
+					fields = { "abbr", "kind", "menu" },
+					format = require("lspkind").cmp_format({
+						mode = "symbol", -- show only symbol annotations
+						maxwidth = 50, -- prevent the popup from showing more than provided characters
+						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+					}),
+				},
+			})
+		end,
+	},
 }
