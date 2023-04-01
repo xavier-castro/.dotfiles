d057868a0f86bebd807f2ed4378a794b7a5ede0e
xavier-castro <xavier_castro@icloud.com>
2023-03-31 19:11:15 -0500

    Add keymaps and bindings for nvim and tmux.
    - Update dependencies in `lazy-lock.json` for the `ChatGPT.nvim` package.
    - Add keymaps for various Telescope commands in `keymaps.lua`.
    - Modify keymaps for `<leader>fb` and `<C-f>` in `keymaps.lua`.
    - Add bindings in `tmux.conf` for creating a new session, displaying a popup menu and running commands.
    - Update bindings in `tmux.conf` for session switching and renaming.
    
    [nvim/.config/nvim/lazy-lock.json]
    - Update the commit hash for the `ChatGPT.nvim` dependency.
    [nvim/.config/nvim/lua/xavier/util/colors.lua]
    - Add a keymapping for `xavier.util.colors.ColorMyPencils`
    - Export `ColorMyPencils` as part of `xavier.util.colors` module
    [nvim/.config/nvim/lua/xavier/config/keymaps.lua]
    - Add keymaps for various Telescope commands including marks, man_pages and live_grep.
    - Remove duplicate keymap for `<leader>F`.
    - Remove keymap for `<leader>f`, which was replaced by the Telescope commands.
    - Modify keymap for `<leader>fb` to open Telescope buffers.
    - Modify keymap for `<C-f>` to open tmux-sessionizer in a new window.
    - Update comments for various keymap assignments.
    [tmux/.config/tmux/tmux.conf]
    - Add a binding for creating a new session at todo
    - Add a binding for pop-up window menu to run selected command
    - Add binding for creating a new session
    - Add multiple new bindings using M and C keys
    - Replace a binding with rename-session command
    - Modify the binding for displaying popup by replacing `tmux switch-client` with `tmux display-popup`
    - Modify the binding for session-switcher by displaying popup with the help of `tmux display-popup` instead of `fzf` command

--- nvim/.config/nvim/lua/xavier/util/colors.lua
+++ nvim/.config/nvim/lua/xavier/util/colors.lua
@@ -1,12 +1,20 @@
+local M = {}
 -- Set colorscheme after options
-vim.cmd[[colorscheme neosolarized]]
+vim.cmd([[colorscheme neosolarized]])
 
-function ColorMyPencils(color)
-	color = color or "neosolarized"
-	vim.cmd.colorscheme(color)
+function M.ColorMyPencils(color)
+  color = color or "neosolarized"
+  vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils()
+M.ColorMyPencils()
+
+
+vim.keymap.set("n", "<leader>8", ":lua require('xavier.util.colors').ColorMyPencils()<CR>",
+{ noremap = true, silent = true })
+
+return M
+
