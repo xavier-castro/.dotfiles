cb5b3943f7016ab0859aed218d68df65dd88df5d
xavier-castro <xavier_castro@icloud.com>
2023-04-08 14:43:58 -0500

    Refactor configuration files and plugins across editors
    - Refactor multiple plugin commit hashes in the `nvim/.config/nvim/lazy-lock.json` file.
    - Update tmux configuration file with new `tmux/.config/tmux/.tmux-cht-command` and `tmux/.config/tmux/.tmux-cht-languages` files.
    - Modify `nvim/.config/nvim/lua/xavier/util/colors.lua` to use `rose-pine` colorscheme.
    - Make miscellaneous updates to other files including file renaming and removal of `lualine.lua`.
    
    [nvim/.config/nvim/lua/xavier/plugins/undotree.lua]
    - Change the outermost container from braces to parentheses.
    [tmux/.config/tmux/.tmux-cht-command]
    - Added a new file `tmux/.config/tmux/.tmux-cht-command`.
    - Added multiple commonly used terminal commands to the new file.
    [nvim/.config/nvim/lua/xavier/plugins/lualine.lua]
    - Delete the `lualine.lua` file entirely.
    [nvim/.config/nvim/lua/xavier/plugins/colorscheme.lua]
    - Remove neosolarized.nvim colorscheme
    - Update the rose-pine colorscheme options to disable background and italics
    [nvim/.config/nvim/lua/xavier/util/colors.lua]
    - Change colorscheme from `neosolarized` to `rose-pine`
    - Refactor `ColorMyPencils` function to use the input color parameter instead of hardcoded value.
    [nvim/.config/nvim/lazy-lock.json]
    - Update the commit hash for `cmp-buffer`, `cmp-cmdline`, `cmp-nvim-lsp`, `cmp-nvim-lua` and `cmp-path` plugins
    - Update the commit hash for `lspsaga.nvim` and `dashboard-nvim` plugins
    - Update the commit hash for `mason.nvim` plugin and remove `colorbuddy.nvim` and `neosolarized.nvim`
    - Update the commit hash for `nvim-lspconfig` and `nvim-navbuddy` plugins
    - Update the commit hash for `nvim-ts-autotag` and `nvim-web-devicons` plugins
    - Update the commit hash for `nvim-treesitter` and `Comment.nvim` plugins
    - Update the commit hash for `toggleterm.nvim` and `LuaSnip` plugins
    [tmux/.config/tmux/.tmux-cht-languages]
    - Added a tmux configuration file with 21 language entries.
    [tmux/.config/tmux/.tmux.conf]
    - Change the `default-terminal` to `"xterm-256color"`
    - Bind the `r` key to a new source-file path
    - Change the `base-index` to `1`
    - Remove bindings to run-shell scripts and assign new ones to new scripts.

--- nvim/.config/nvim/lua/xavier/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/xavier/plugins/colorscheme.lua
@@ -3,56 +3,9 @@ return { -- neoosolarized
 		"rose-pine/neovim",
 		lazy = false,
 		name = "rose-pine",
-	},
-	{
-		"svrana/neosolarized.nvim",
-		lazy = false,
-		dependencies = { "tjdevries/colorbuddy.nvim" },
-		config = function()
-			local status, n = pcall(require, "neosolarized")
-			if not status then
-				return
-			end
-
-			n.setup({
-				comment_italics = true,
-			})
-
-			local cb = require("colorbuddy.init")
-			local Color = cb.Color
-			local colors = cb.colors
-			local Group = cb.Group
-			local groups = cb.groups
-			local styles = cb.styles
-
-			Color.new("white", "#ffffff")
-			Color.new("black", "#000000")
-			Group.new("Normal", colors.base1, colors.NONE, styles.NONE)
-			Group.new("CursorLine", colors.none, colors.base03, styles.NONE, colors.base1)
-			Group.new("CursorLineNr", colors.yellow, colors.black, styles.NONE, colors.base1)
-			Group.new("Visual", colors.none, colors.base03, styles.reverse)
-			Group.new("CopilotSuggestion", colors.yellow, colors.NONE, styles.NONE)
-
-			local cError = groups.Error.fg
-			local cInfo = groups.Information.fg
-			local cWarn = groups.Warning.fg
-			local cHint = groups.Hint.fg
-
-			Group.new("DiagnosticVirtualTextError", cError, cError:dark():dark():dark():dark(), styles.NONE)
-			Group.new("DiagnosticVirtualTextInfo", cInfo, cInfo:dark():dark():dark(), styles.NONE)
-			Group.new("DiagnosticVirtualTextWarn", cWarn, cWarn:dark():dark():dark(), styles.NONE)
-			Group.new("DiagnosticVirtualTextHint", cHint, cHint:dark():dark():dark(), styles.NONE)
-			Group.new("DiagnosticUnderlineError", colors.none, colors.none, styles.undercurl, cError)
-			Group.new("DiagnosticUnderlineWarn", colors.none, colors.none, styles.undercurl, cWarn)
-			Group.new("DiagnoticUnderlineInfo", colors.none, colors.none, styles.undercurl, cInfo)
-			Group.new("DiagnosticUnderlineHint", colors.none, colors.none, styles.undercurl, cHint)
-
-			Group.new("HoverBorder", colors.yellow, colors.none, styles.NONE)
-		end,
-	}, -- rosepine
-
-	{
-		"projekt0n/github-nvim-theme",
-		lazy = false,
+		opts = {
+			disable_background = true,
+			disable_italics = true,
+		},
 	},
 }
--- nvim/.config/nvim/lua/xavier/util/colors.lua
+++ nvim/.config/nvim/lua/xavier/util/colors.lua
@@ -1,9 +1,9 @@
 local M = {}
 -- Set colorscheme after options
-vim.cmd([[colorscheme neosolarized]])
+vim.cmd([[colorscheme rose-pine]])
 
 function M.ColorMyPencils(color)
-	color = color or "neosolarized"
+	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
