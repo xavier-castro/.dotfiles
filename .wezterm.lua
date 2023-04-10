f2ea52a3e5cb372687f529eac99e3458859cc3be
xavier-castro <xavier_castro@icloud.com>
2023-04-10 14:39:03 -0500

    Add new plugins and configurations to nvim.
    - Add and configure new plugins (`tabnine`, `CodeGPT.nvim`, and `codeium.nvim`) to `nvim`
    - Update options for `nvim` to include `@` in `isfname` and add two new autocommands
    - Update `wezterm` color scheme to use "rose-pine"
    - Update `lazy-lock.json` to include newly added plugins
    - No other relevant changes in the commit.
    
    [nvim/.config/nvim/lua/xavier/plugins/tabnine.lua]
    - Add `tabnine` plugin configuration for `nvim`
    [nvim/.config/nvim/lua/xavier/config/options.lua]
    - Set `isfname` to include the character `@`
    - Add 2 autocommands:
      - Make `GitSigns` have a transparent background
      - Make `Neovim` remember folds
    [.wezterm.lua]
    - Comment out the previous color scheme and add a new one called "rose-pine"
    [nvim/.config/nvim/lua/xavier/plugins/codegpt.lua]
    - Add `CodeGPT.nvim` as a plugin.
    - Add two dependencies, `nvim-lua/plenary.nvim` and `MunifTanjim/nui.nvim`.
    - Call the `codegpt.config` function on startup.
    [nvim/.config/nvim/lua/xavier/plugins/codeium.lua]
    - Add a new file `codeium.lua` to `xavier/plugins`.
    - `codeium.lua` exports a table with two keys, and their values are tables containing configurations.
    - The first configuration contains `http.nvim` as its key and `cargo build --workspace --release` as its build value.
    - The second configuration contains `codeium.nvim` as its key and a dependencies key with `http.nvim`, `plenary.nvim`, and `nvim-cmp` as its values.
    - The second configuration's value also includes a `config` key with a function as its value.
    [nvim/.config/nvim/lazy-lock.json]
    - Add the `CodeGPT.nvim` plugin to the `lazy-lock.json` file
    - Add the `codeium.nvim` plugin to the `lazy-lock.json` file
    - Add the `http.nvim` plugin to the `lazy-lock.json` file
    - Add the `lspkind.nvim` plugin to the `lazy-lock.json` file
    - Add the `tabnine-nvim` plugin to the `lazy-lock.json` file
    - No other relevant changes to other files in the commit.

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,8 @@ config.font = wezterm.font_with_fallback{
     'Hack Nerd Font Mono',
 }
 config.font_size = 16
-config.color_scheme = "Dark+"
+-- config.color_scheme = "Dark+"
+config.color_scheme = "rose-pine"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.92
 config.macos_window_background_blur = 20
--- nvim/.config/nvim/lua/xavier/config/options.lua
+++ nvim/.config/nvim/lua/xavier/config/options.lua
@@ -2,30 +2,41 @@ vim.opt.guicursor = ""
 vim.opt.clipboard = "unnamedplus"
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
 vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
+
+-- Autocommands
+-- Make GitSigns transparent background
+vim.cmd([[
+  augroup GitSignsHighlights
+    autocmd!
+    autocmd ColorScheme * highlight GitSignsAdd guibg=NONE
+    autocmd ColorScheme * highlight GitSignsChange guibg=NONE
+    autocmd ColorScheme * highlight GitSignsDelete guibg=NONE
+  augroup END
+]])
+
+-- Make Neovim remember folds
+vim.cmd([[
+augroup RememberFolds
+    autocmd!
+    au BufWinLeave ?* mkview 1
+    au BufWinEnter ?* silent! loadview 1
+augroup END
+]])
