2d5527ae25951b610fbd4aefd2408130681d5e10
xavier-castro <xavier_castro@icloud.com>
2023-04-12 13:45:14 -0500

    Configure todo comments

--- nvim/.config/nvim/lua/xavier/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/xavier/plugins/todo-comments.lua
@@ -18,7 +18,7 @@ return {
 					HACK = { icon = " ", color = "warning" },
 					WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
 					PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-					NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+					NOTE = { icon = " ", color = "hint", alt = { "INFO", "MARK" } },
 					TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
 				},
 				gui_style = {
