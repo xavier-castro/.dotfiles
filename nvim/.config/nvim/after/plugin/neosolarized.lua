c457478f3802778fec8019deff1fbedc9e006e70
xavier-castro <xavier_castro@icloud.com>
2023-05-03 17:58:45 -0500

    fr

--- nvim/.config/nvim/after/plugin/neosolarized.lua
+++ nvim/.config/nvim/after/plugin/neosolarized.lua
@@ -2,9 +2,11 @@ local status, n = pcall(require, "neosolarized")
 if not status then
 	return
 end
+
 n.setup({
 	comment_italics = true,
 })
+
 local cb = require("colorbuddy.init")
 local Color = cb.Color
 local colors = cb.colors
@@ -31,6 +33,7 @@ Group.new("DiagnosticUnderlineWarn", colors.none, colors.none, styles.undercurl,
 Group.new("DiagnosticUnderlineInfo", colors.none, colors.none, styles.undercurl, cInfo)
 Group.new("DiagnosticUnderlineHint", colors.none, colors.none, styles.undercurl, cHint)
 Group.new("HoverBorder", colors.yellow, colors.none, styles.NONE)
+
 function ColorMyPencils(color)
 	color = color or "neosolarized"
 	vim.cmd.colorscheme(color)
@@ -41,4 +44,5 @@ function ColorMyPencils(color)
 		bg = "none",
 	})
 end
+
 ColorMyPencils()
--- nvim/.config/nvim/lua/xavier/plugins/init.lua
+++ nvim/.config/nvim/lua/xavier/plugins/init.lua
@@ -1,194 +1,194 @@
 return {
-  {
-    "svrana/neosolarized.nvim",
-    dependencies = { "tjdevries/colorbuddy.nvim" },
-    lazy = false,
-  },
-  {
-    "AckslD/swenv.nvim",
-    config = function()
-      require("swenv").setup({
-        -- Should return a list of tables with a `name` and a `path` entry each.
-        -- Gets the argument `venvs_path` set below.
-        -- By default just lists the entries in `venvs_path`.
-        get_venvs = function(venvs_path)
-          return require("swenv.api").get_venvs(venvs_path)
-        end,
-        -- Path passed to `get_venvs`.
-        venvs_path = vim.fn.expand("~/venvs"),
-        -- Something to do after setting an environment
-        post_set_venv = nil,
-      })
-    end,
-  },
-  { "nvim-pack/nvim-spectre" },
-  { "mrjones2014/nvim-ts-rainbow" },
-  {
-    "willothy/veil.nvim",
-    lazy = true,
-    dependencies = {
-      -- All optional, only required for the default setup.
-      -- If you customize your config, these aren't necessary.
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-file-browser.nvim",
-    },
-    config = true,
-    -- or configure with:
-    -- opts = { ... }
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-    config = function()
-      vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>")
-      vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>")
-      vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>")
-      vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>")
-    end,
-  },
-  { "tpope/vim-surround" },
-  { "tpope/vim-repeat" },
-  { "tpope/vim-fugitive" },
-  { "tpope/vim-commentary" },
-  { "RRethy/vim-illuminate" },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  },
-  { "norcalli/nvim-colorizer.lua" },
-  { "kshenoy/vim-signature" },
-  { "justinmk/vim-sneak" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    dependencies = {
-      { "nvim-treesitter/playground" },
-      { "nvim-treesitter/nvim-treesitter-context" },
-    },
-  },
-  { "theprimeagen/harpoon" },
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  { "theprimeagen/refactoring.nvim" },
-  { "mbbill/undotree" },
-  { "folke/trouble.nvim" },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      { "sar/web-devicons.nvim" },
-      { "nvim-telescope/telescope-file-browser.nvim" },
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope-ui-select.nvim" },
-      { "nvim-telescope/telescope-fzf-native.nvim",  build = "make" },
-    },
-  }, -- Bufferline + Lualine
-  { "akinsho/nvim-bufferline.lua" },
-  { "nvim-lualine/lualine.nvim" },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-    },
-    event = "InsertEnter",
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    lazy = false,
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lua",
-      "windwp/nvim-autopairs",
-      "onsails/lspkind-nvim",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "folke/neodev.nvim",
-      "nvim-lua/lsp-status.nvim",
-      "jose-elias-alvarez/typescript.nvim",
-      "b0o/schemastore.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "MunifTanjim/prettier.nvim",
-    },
-    event = { "BufReadPre", "BufNewFile" },
-  },
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-  },
-  { "jose-elias-alvarez/null-ls.nvim" },
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
-  { "glepnir/lspsaga.nvim",             event = "LspAttach" },
-  { "windwp/nvim-autopairs" },
-  { "windwp/nvim-ts-autotag" },
-  { "github/copilot.vim" },
-  { "akinsho/nvim-toggleterm.lua" },
-  {
-    "stevearc/aerial.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
-  {
-    "petertriho/nvim-scrollbar",
-    config = function()
-      local scrollbar = require("scrollbar")
-      scrollbar.setup({})
-    end,
-  },
-  {
-    "Pocco81/true-zen.nvim",
-    keys = {
-      {
-        "<leader>zz",
-        function()
-          local truezen = require("true-zen")
-          truezen.ataraxis()
-        end,
-        desc = "zen",
-      },
-    },
-  },
-  {
-    "numToStr/Comment.nvim",
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-  },
+	{
+		"svrana/neosolarized.nvim",
+		dependencies = { "tjdevries/colorbuddy.nvim" },
+		lazy = false,
+	},
+	{
+		"AckslD/swenv.nvim",
+		config = function()
+			require("swenv").setup({
+				-- Should return a list of tables with a `name` and a `path` entry each.
+				-- Gets the argument `venvs_path` set below.
+				-- By default just lists the entries in `venvs_path`.
+				get_venvs = function(venvs_path)
+					return require("swenv.api").get_venvs(venvs_path)
+				end,
+				-- Path passed to `get_venvs`.
+				venvs_path = vim.fn.expand("~/venvs"),
+				-- Something to do after setting an environment
+				post_set_venv = nil,
+			})
+		end,
+	},
+	{ "nvim-pack/nvim-spectre" },
+	{ "mrjones2014/nvim-ts-rainbow" },
+	{
+		"willothy/veil.nvim",
+		lazy = true,
+		dependencies = {
+			-- All optional, only required for the default setup.
+			-- If you customize your config, these aren't necessary.
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		config = true,
+		-- or configure with:
+		-- opts = { ... }
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+		config = function()
+			vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>")
+			vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>")
+			vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>")
+			vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>")
+		end,
+	},
+	{ "tpope/vim-surround" },
+	{ "tpope/vim-repeat" },
+	{ "tpope/vim-fugitive" },
+	{ "tpope/vim-commentary" },
+	{ "RRethy/vim-illuminate" },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{ "norcalli/nvim-colorizer.lua" },
+	{ "kshenoy/vim-signature" },
+	{ "justinmk/vim-sneak" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			{ "nvim-treesitter/playground" },
+			{ "nvim-treesitter/nvim-treesitter-context" },
+		},
+	},
+	{ "theprimeagen/harpoon" },
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{ "theprimeagen/refactoring.nvim" },
+	{ "mbbill/undotree" },
+	{ "folke/trouble.nvim" },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{ "sar/web-devicons.nvim" },
+			{ "nvim-telescope/telescope-file-browser.nvim" },
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+	}, -- Bufferline + Lualine
+	{ "akinsho/nvim-bufferline.lua" },
+	{ "nvim-lualine/lualine.nvim" },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+		},
+		event = "InsertEnter",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		lazy = false,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lua",
+			"windwp/nvim-autopairs",
+			"onsails/lspkind-nvim",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"folke/neodev.nvim",
+			"nvim-lua/lsp-status.nvim",
+			"jose-elias-alvarez/typescript.nvim",
+			"b0o/schemastore.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"MunifTanjim/prettier.nvim",
+		},
+		event = { "BufReadPre", "BufNewFile" },
+	},
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = "kevinhwang91/promise-async",
+	},
+	{ "jose-elias-alvarez/null-ls.nvim" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "glepnir/lspsaga.nvim", event = "LspAttach" },
+	{ "windwp/nvim-autopairs" },
+	{ "windwp/nvim-ts-autotag" },
+	{ "github/copilot.vim" },
+	{ "akinsho/nvim-toggleterm.lua" },
+	{
+		"stevearc/aerial.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"petertriho/nvim-scrollbar",
+		config = function()
+			local scrollbar = require("scrollbar")
+			scrollbar.setup({})
+		end,
+	},
+	{
+		"Pocco81/true-zen.nvim",
+		keys = {
+			{
+				"<leader>zz",
+				function()
+					local truezen = require("true-zen")
+					truezen.ataraxis()
+				end,
+				desc = "zen",
+			},
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+	},
 
-  {
-    "dpayne/CodeGPT.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("codegpt.config")
-    end,
-  },
+	{
+		"dpayne/CodeGPT.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("codegpt.config")
+		end,
+	},
 
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("indent_blankline").setup({
-        -- for example, context is off by default, use this to turn it on
-        show_current_context = true,
-        -- show_current_context_start = true,
-      })
-    end,
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("indent_blankline").setup({
+				-- for example, context is off by default, use this to turn it on
+				show_current_context = true,
+				-- show_current_context_start = true,
+			})
+		end,
+	},
 }
