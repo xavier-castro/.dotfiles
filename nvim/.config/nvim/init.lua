9ecf50056a1e5df2728187ae64dd48c653d9ba23
xavier-castro <xavier_castro@icloud.com>
2023-05-05 04:21:23 -0500

    Add folding navigation for vscode

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,83 +1,89 @@
 if vim.g.vscode then
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not vim.loop.fs_stat(lazypath) then
-		vim.fn.system({
-			"git",
-			"clone",
-			"--filter=blob:none",
-			"https://github.com/folke/lazy.nvim.git",
-			"--branch=stable",
-			lazypath,
-		})
-	end
-	vim.opt.rtp:prepend(lazypath)
-	vim.g.mapleader = " "
-	vim.keymap.set("x", "<leader>p", '"_dP') -- Your paste will be saved
-	vim.keymap.set("n", "<leader>y", '"+y')
-	vim.keymap.set("v", "<leader>y", '"+y')
-	vim.keymap.set("n", "<leader>Y", '"+Y')
-	vim.keymap.set("n", "<leader>d", '"_d')
-	vim.keymap.set("v", "<leader>d", '"_d')
-	vim.keymap.set("v", "K", ":m '<0<CR>gv=gv")
-	vim.keymap.set("v", "J", ":m '>+3<CR>gv=gv")
-	vim.keymap.set("n", "J", "mzJ`z")
-	vim.keymap.set("n", "<C-a>", "gg<S-v>G")
-	vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:bd!<cr>")
-	vim.keymap.set("i", "<c-s>", "<Esc>:w<CR>a")
-	vim.keymap.set("n", "<c-s>", ":w<CR>")
-	vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-	vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-	vim.keymap.set("n", "<Tab>", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<cr>", {})
-	vim.keymap.set("n", "<S-Tab>", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<cr>", {})
-	vim.keymap.set("i", "<C-c>", "<Esc>")
-	vim.keymap.set("i", "<C-o>", "<C-o> <esc>o")
-	vim.keymap.set("n", "<C-a>", "gg<S-v>G")
-	vim.opt.shell = "fish"
-	vim.opt.background = "dark"
-	vim.opt.updatetime = 50
-	vim.opt.inccommand = "split"
-	vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
-	-- highlight yanked text for 200ms using the "Visual" highlight group
-	vim.cmd([[
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+  vim.g.mapleader = " "
+  vim.cmd([[
+" Workaround for gk/gj
+nnoremap gk :<C-u>call VSCodeCall('cursorMove', { 'to': 'up', 'by': 'wrappedLine', 'value': v:count ? v:count : 1 })<CR>
+nnoremap gj :<C-u>call VSCodeCall('cursorMove', { 'to': 'down', 'by': 'wrappedLine', 'value': v:count ? v:count : 1 })<CR>
+  ]])
+  vim.keymap.set("n", "<leader>y", '"+y')
+  vim.keymap.set("v", "<leader>y", '"+y')
+  vim.keymap.set("n", "<leader>Y", '"+Y')
+  vim.keymap.set("n", "<leader>d", '"_d')
+  vim.keymap.set("v", "<leader>d", '"_d')
+  vim.keymap.set("v", "K", ":m '<0<CR>gv=gv")
+  vim.keymap.set("v", "J", ":m '>+3<CR>gv=gv")
+  vim.keymap.set("n", "J", "mzJ`z")
+  vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+  vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:bd!<cr>")
+  vim.keymap.set("i", "<c-s>", "<Esc>:w<CR>a")
+  vim.keymap.set("n", "<c-s>", ":w<CR>")
+  vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+  vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+  vim.keymap.set("n", "<Tab>", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<cr>", {})
+  vim.keymap.set("n", "<S-Tab>", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<cr>", {})
+  vim.keymap.set("i", "<C-c>", "<Esc>")
+  vim.keymap.set("i", "<C-o>", "<C-o> <esc>o")
+  vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+  vim.opt.shell = "fish"
+  vim.opt.background = "dark"
+  vim.opt.updatetime = 50
+  vim.opt.inccommand = "split"
+  vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+  -- highlight yanked text for 200ms using the "Visual" highlight group
+  vim.cmd([[
   augroup highlight_yank
   autocmd!
   au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
   augroup END
 ]])
-	require("lazy").setup({
-		{ "machakann/vim-sandwich" },
-		{
-			"tpope/vim-surround",
-		},
-		{
-			"tpope/vim-repeat",
-		},
-		{
-			"justinmk/vim-sneak",
-		},
-		{
-			"tpope/vim-fugitive",
-		},
-		{
-			"tpope/vim-commentary",
-		},
-	})
-else
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not vim.loop.fs_stat(lazypath) then
-		vim.fn.system({
-			"git",
-			"clone",
-			"--filter=blob:none",
-			"https://github.com/folke/lazy.nvim.git",
-			"--branch=stable",
-			lazypath,
-		})
-	end
-	vim.opt.rtp:prepend(lazypath)
-	vim.g.mapleader = " "
+  require("lazy").setup({
+    { "machakann/vim-sandwich" },
+    {
+      "tpope/vim-surround",
+    },
+    {
+      "phaazon/hop.nvim",
+      branch = "v2",
+    },
+    {
+      "tpope/vim-repeat",
+    },
+    {
+      "justinmk/vim-sneak",
+    },
 
-	require("xavier.config.set")
-	require("xavier.config.remap");
-	(require("lazy")).setup("xavier.plugins")
+    {
+      "tpope/vim-commentary",
+    },
+  })
+else
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+  vim.g.mapleader = " "
+
+  require("xavier.config.set")
+  require("xavier.config.remap");
+  (require("lazy")).setup("xavier.plugins")
 end
