9463b0100e6e5eef5bf93c1ef8ee2d0f39b0fbff
xavier-castro <xavier_castro@icloud.com>
2023-05-05 15:18:06 -0500

    Reenable alt tab because im use to it now

--- nvim/.config/nvim/after/plugin/alpha.lua
+++ nvim/.config/nvim/after/plugin/alpha.lua
@@ -1,33 +1,37 @@
-local alpha = require("alpha")
-local dashboard = require("alpha.themes.dashboard")
+if vim.g.vscode then
+	return
+else
+	local alpha = require("alpha")
+	local dashboard = require("alpha.themes.dashboard")
 
--- Set header
-dashboard.section.header.val = {
-	"                                                     ",
-	"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-	"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-	"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-	"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-	"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-	"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-	"                                                     ",
-}
+	-- Set header
+	dashboard.section.header.val = {
+		"                                                     ",
+		"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+		"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+		"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+		"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+		"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+		"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+		"                                                     ",
+	}
 
--- Set menu
-dashboard.section.buttons.val = {
-	dashboard.button("R", "🚀  Restore Session", ":SessionManager load_last_session<CR>"),
-	dashboard.button("S", "📗  Browse Sessions", ":SessionManager load_session<CR>"),
-	dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("f", "   Browse Files", ":Telescope fd<CR>"),
-	dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
-	dashboard.button("s", "   Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-	dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
-}
+	-- Set menu
+	dashboard.section.buttons.val = {
+		dashboard.button("R", "🚀  Restore Session", ":SessionManager load_last_session<CR>"),
+		dashboard.button("S", "📗  Browse Sessions", ":SessionManager load_session<CR>"),
+		dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
+		dashboard.button("f", "   Browse Files", ":Telescope fd<CR>"),
+		dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+		dashboard.button("s", "   Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+		dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
+	}
 
--- Send config to alpha
-alpha.setup(dashboard.opts)
+	-- Send config to alpha
+	alpha.setup(dashboard.opts)
 
--- Disable folding on alpha buffer
-vim.cmd([[
+	-- Disable folding on alpha buffer
+	vim.cmd([[
     autocmd FileType alpha setlocal nofoldenable
 ]])
+end
