efb7501bbaa023e3370c70ced4a2efa901f901e9
xavier-castro <xavier_castro@icloud.com>
2023-09-28 12:49:00 -0500

    Setup for changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -81,29 +81,30 @@ nnoremap gj :<C-u>call VSCodeCall('cursorMove', { 'to': 'down', 'by': 'wrappedLi
 
 		{ "tpope/vim-commentary" },
 	})
+	-- Multi color highlighting in VSC
 	vim.api.nvim_exec(
-		[[
-    " THEME CHANGER
-    function! SetCursorLineNrColorInsert(mode)
-        " Insert mode: blue
-        if a:mode == "i"
-            call VSCodeNotify('nvim-theme.insert')
+		-- [[
+    -- " THEME CHANGER
+    -- function! SetCursorLineNrColorInsert(mode)
+        -- " Insert mode: blue
+        -- if a:mode == "i"
+            -- call VSCodeNotify('nvim-theme.insert')
 
-        " Replace mode: red
-        elseif a:mode == "r"
-            call VSCodeNotify('nvim-theme.replace')
-        endif
-    endfunction
+        -- " Replace mode: red
+        -- elseif a:mode == "r"
+            -- call VSCodeNotify('nvim-theme.replace')
+        -- endif
+    -- endfunction
 
-    augroup CursorLineNrColorSwap
-        autocmd!
-        autocmd ModeChanged *:[vV\x16]* call VSCodeNotify('nvim-theme.visual')
-        autocmd ModeChanged *:[R]* call VSCodeNotify('nvim-theme.replace')
-        autocmd InsertEnter * call SetCursorLineNrColorInsert(v:insertmode)
-        autocmd InsertLeave * call VSCodeNotify('nvim-theme.normal')
-        autocmd CursorHold * call VSCodeNotify('nvim-theme.normal')
-    augroup END
-]],
+    -- augroup CursorLineNrColorSwap
+        -- autocmd!
+        -- autocmd ModeChanged *:[vV\x16]* call VSCodeNotify('nvim-theme.visual')
+        -- autocmd ModeChanged *:[R]* call VSCodeNotify('nvim-theme.replace')
+        -- autocmd InsertEnter * call SetCursorLineNrColorInsert(v:insertmode)
+        -- autocmd InsertLeave * call VSCodeNotify('nvim-theme.normal')
+        -- autocmd CursorHold * call VSCodeNotify('nvim-theme.normal')
+    -- augroup END
+-- ]],
 		false
 	)
 else
