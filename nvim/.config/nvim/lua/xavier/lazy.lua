b17eabcda1980c9df3f3af6b522bc3a7793fb645
xavier-castro <xavier_castro@icloud.com>
2023-11-11 18:14:58 -0600

    Edit neotree

--- nvim/.config/nvim/lua/xavier/lazy.lua
+++ nvim/.config/nvim/lua/xavier/lazy.lua
@@ -92,15 +92,6 @@ require('lazy').setup({
   { import = 'xavier.plugins.lsp' },
 }, {})
 
--- [[ Basic Keymaps ]]
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/.config/nvim/lua/xavier/remap.lua
+++ nvim/.config/nvim/lua/xavier/remap.lua
@@ -1,7 +1,17 @@
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
 local utils = require 'xavier.utils'
 
 -- Open vim explorer [replaced by neo-tree]
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Move selected lines around
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
