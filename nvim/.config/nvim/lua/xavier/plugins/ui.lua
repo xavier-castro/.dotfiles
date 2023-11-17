e458fd8af8b78f3f55f9c472aa73706358eb9491
xavier-castro <xavier_castro@icloud.com>
2023-11-17 15:16:48 -0600

    make bufferline not always show

--- nvim/.config/nvim/lua/xavier/plugins/ui.lua
+++ nvim/.config/nvim/lua/xavier/plugins/ui.lua
@@ -141,6 +141,7 @@ return {
       options = {
         mode = "tabs",
         -- separator_style = "slant",
+        always_show_bufferline = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
       },
