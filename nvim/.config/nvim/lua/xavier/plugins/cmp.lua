a65fdf6097c9883278bd58ea9adad71816f5ad2f
xavier-castro <xavier_castro@icloud.com>
2023-11-30 13:26:36 -0600

    replace lsp with lsp-zero

--- nvim/.config/nvim/lua/xavier/plugins/cmp.lua
+++ nvim/.config/nvim/lua/xavier/plugins/cmp.lua
@@ -1,77 +1,78 @@
 return {
- {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
+    {
+        -- Autocompletion
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lsp",
+            "rafamadriz/friendly-snippets",
+        },
+        config = function()
+            lsp_zero = require("lsp-zero")
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            require("luasnip.loaders.from_vscode").lazy_load()
+            luasnip.config.setup({})
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<M-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<M-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-f>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.close(),
+                    ["<Tab>"] = nil,
+                    ["<S-Tab>"] = nil,
+                    ["<C-j>"] = function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif require("luasnip").expand_or_jumpable() then
+                            vim.fn.feedkeys(
+                                vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+                                ""
+                            )
+                        else
+                            local copilot_keys = vim.fn["copilot#Accept"]()
+                            if copilot_keys ~= "" then
+                                vim.api.nvim_feedkeys(copilot_keys, "i", true)
+                            else
+                                fallback()
+                            end
+                        end
+                    end,
+
+                    ["<C-k>"] = function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif require("luasnip").jumpable(-1) then
+                            vim.fn.feedkeys(
+                                vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true),
+                                ""
+                            )
+                        else
+                            fallback()
+                        end
+                    end,
+                }),
+                sources = {
+                    { name = "path" },
+                    { name = "nvim_lsp" },
+                    { name = "nvim_lua" },
+                },
+                formatting = lsp_zero.cmp_format(),
+            })
+        end,
     },
-config=function()
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<M-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<M-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-f>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<Tab>"] = nil,
-    ["<S-Tab>"] = nil,
-    ["<C-j>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(
-          vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-          ""
-        )
-      else
-        local copilot_keys = vim.fn["copilot#Accept"]()
-        if copilot_keys ~= "" then
-          vim.api.nvim_feedkeys(copilot_keys, "i", true)
-        else
-          fallback()
-        end
-      end
-    end,
-
-    ["<C-k>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-      else
-        fallback()
-      end
-    end,
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
 }
-end
-
-  },
-  }
--- nvim/.config/nvim/lua/xavier/plugins/themes.lua
+++ nvim/.config/nvim/lua/xavier/plugins/themes.lua
@@ -1,18 +1,18 @@
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				disable_background = true,
-			})
-			vim.cmd.colorscheme("rose-pine")
-		end,
-	},
-	{
-		"andreypopp/vim-colors-plain",
-		config = function()
-			-- vim.cmd.colorscheme("plain")
-		end,
-	},
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+            })
+            vim.cmd.colorscheme("rose-pine")
+        end,
+    },
+    {
+        "andreypopp/vim-colors-plain",
+        config = function()
+            -- vim.cmd.colorscheme("plain")
+        end,
+    },
 }
