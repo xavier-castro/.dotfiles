89130d0c5de54a6ee11efa250bd1977d20c538f7
xavier-castro <xavier_castro@icloud.com>
2023-11-29 18:12:38 -0600

    fix rose-pine

--- nvim/.config/nvim/lua/xavier/plugins/rose-pine.lua
+++ nvim/.config/nvim/lua/xavier/plugins/rose-pine.lua
@@ -1,5 +1,13 @@
 return {
-    { 'rose-pine/neovim', name = 'rose-pine', opts={disable_background=true}, config=function()
-        vim.cmd.colorscheme('rose-pine')
-          end },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+            })
+			vim.cmd.colorscheme("rose-pine")
+		end,
+	},
 }
+
