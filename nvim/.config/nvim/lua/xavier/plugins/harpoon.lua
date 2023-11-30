b0429ee44dee9f5f45659536fa48cfc15a3c427a
xavier-castro <xavier_castro@icloud.com>
2023-11-29 20:27:12 -0600

    fix the mess of alacritty

--- nvim/.config/nvim/lua/xavier/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/xavier/plugins/harpoon.lua
@@ -1,7 +1,7 @@
 return {
-	{
+    {
 		"theprimeagen/harpoon",
-		event = "VeryLazy",
+        event="VeryLazy",
 		keys = {
 			{
 				";e",
@@ -15,16 +15,16 @@ return {
 			local mark = require("harpoon.mark")
 			local ui = require("harpoon.ui")
 			vim.keymap.set("n", "<leader>a", mark.add_file)
-			vim.keymap.set("n", "˙", function()
+			vim.keymap.set("n", "<M-h>", function()
 				ui.nav_file(1)
 			end)
-			vim.keymap.set("n", "∆", function()
+			vim.keymap.set("n", "<M-j>", function()
 				ui.nav_file(2)
 			end)
-			vim.keymap.set("n", "˚", function()
+			vim.keymap.set("n", "<M-k>", function()
 				ui.nav_file(3)
 			end)
-			vim.keymap.set("n", "¬", function()
+			vim.keymap.set("n", "<M-l>", function()
 				ui.nav_file(4)
 			end)
 		end,
--- nvim/.config/nvim/lua/xavier/set.lua
+++ nvim/.config/nvim/lua/xavier/set.lua
@@ -30,7 +30,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-
+vim.opt.cursorline = true
 vim.opt.colorcolumn = "80"
 
 vim.g.netrw_browse_split = 0
