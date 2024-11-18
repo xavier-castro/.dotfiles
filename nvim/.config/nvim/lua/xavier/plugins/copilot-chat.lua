b4b5f07737e3be7ffc701bde966c728525b89292
Xavier Castro <xavier@Xaviers-MBP.lan>
2024-11-17 21:24:54 -0600

    Add CopilotChat

---
+++ nvim/.config/nvim/lua/xavier/plugins/copilot-chat.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
--- nvim/.config/nvim/lua/xavier/plugins/copilot.lua
+++ nvim/.config/nvim/lua/xavier/plugins/copilot.lua
@@ -1,33 +1,33 @@
 return {
 	{
-		-- "github/copilot.vim",
-		-- config = function()
-		-- 	vim.g.copilot_filetypes = {
-		-- 		TelescopePrompt = false,
-		-- 	}
-		-- 	vim.keymap.set({ "i", "n" }, "<M-p>", ":Copilot panel<cr>")
-		-- 	vim.keymap.set("i", "<C-a>", 'copilot#Accept("<CR>")', {
-		-- 		expr = true,
-		-- 		replace_keycodes = false,
-		-- 	})
-		-- 	vim.g.copilot_no_tab_map = true
-		-- end,
-
-		"zbirenbaum/copilot.lua",
+		"github/copilot.vim",
 		config = function()
-			local copilot = require("copilot")
-			copilot.setup({
-				suggestion = {
-					auto_trigger = true,
-					keymap = {
-						accept = false,
-					},
-				},
-				filetypes = {
-					markdown = true,
-					help = true,
-				},
+			vim.g.copilot_filetypes = {
+				TelescopePrompt = false,
+			}
+			vim.keymap.set({ "i", "n" }, "<M-p>", ":Copilot panel<cr>")
+			vim.keymap.set("i", "<C-a>", 'copilot#Accept("<CR>")', {
+				expr = true,
+				replace_keycodes = false,
 			})
+			vim.g.copilot_no_tab_map = true
 		end,
+
+		-- "zbirenbaum/copilot.lua",
+		-- config = function()
+		-- 	local copilot = require("copilot")
+		-- 	copilot.setup({
+		-- 		suggestion = {
+		-- 			auto_trigger = true,
+		-- 			keymap = {
+		-- 				accept = false,
+		-- 			},
+		-- 		},
+		-- 		filetypes = {
+		-- 			markdown = true,
+		-- 			help = true,
+		-- 		},
+		-- 	})
+		-- end,
 	},
 }
