6be41dd1fef3974316bfb899b87130a445c217d0
xavier-castro <xavier_castro@icloud.com>
2024-12-11 16:46:15 -0600

    add more themes

--- nvim/.config/nvim/lua/xavier/plugins/themes.lua
+++ nvim/.config/nvim/lua/xavier/plugins/themes.lua
@@ -54,36 +54,8 @@ return {
 	},
 	-- Gruvbox
 	{
-		"ellisonleao/gruvbox.nvim",
+		"luisiacc/gruvbox-baby",
 		priority = 1000,
-		config = function()
-			-- Default options:
-			require("gruvbox").setup({
-				terminal_colors = true, -- add neovim terminal colors
-				undercurl = true,
-				underline = true,
-				bold = true,
-				italic = {
-					strings = true,
-					emphasis = true,
-					comments = true,
-					operators = false,
-					folds = true,
-				},
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				invert_intend_guides = false,
-				inverse = true, -- invert background for search, diffs, statuslines and errors
-				contrast = "", -- can be "hard", "soft" or empty string
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = false,
-			})
-			-- vim.cmd("colorscheme gruvbox")
-		end,
 	},
 	{
 		"catppuccin/nvim",
@@ -186,7 +158,121 @@ return {
 					background = true, -- use background color for virtual text
 				},
 			})
-			vim.cmd.colorscheme("onedark")
+			-- vim.cmd.colorscheme("onedark")
+		end,
+	},
+	{
+		"EdenEast/nightfox.nvim",
+		config = function()
+			-- Default options
+			require("nightfox").setup({
+				options = {
+					-- Compiled file's destination location
+					compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+					compile_file_suffix = "_compiled", -- Compiled file suffix
+					transparent = true, -- Disable setting background
+					terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+					dim_inactive = false, -- Non focused panes set to alternative background
+					module_default = true, -- Default enable value for modules
+					colorblind = {
+						enable = false, -- Enable colorblind support
+						simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
+						severity = {
+							protan = 0, -- Severity [0,1] for protan (red)
+							deutan = 0, -- Severity [0,1] for deutan (green)
+							tritan = 0, -- Severity [0,1] for tritan (blue)
+						},
+					},
+					styles = { -- Style to be applied to different syntax groups
+						comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+						conditionals = "NONE",
+						constants = "NONE",
+						functions = "NONE",
+						keywords = "NONE",
+						numbers = "NONE",
+						operators = "NONE",
+						strings = "NONE",
+						types = "NONE",
+						variables = "NONE",
+					},
+					inverse = { -- Inverse highlight for different types
+						match_paren = false,
+						visual = false,
+						search = false,
+					},
+					modules = { -- List of various plugins and additional options
+						-- ...
+					},
+				},
+				palettes = {},
+				specs = {},
+				groups = {},
+			})
+
+			-- setup must be called before loading
+			-- vim.cmd("colorscheme nightfox")
+		end,
+	},
+	-- Lazy
+	{
+		"vague2k/vague.nvim",
+		config = function()
+			require("vague").setup({
+				transparent = true, -- don't set background
+				style = {
+					-- "none" is the same thing as default. But "italic" and "bold" are also valid options
+					boolean = "none",
+					number = "none",
+					float = "none",
+					error = "none",
+					comments = "italic",
+					conditionals = "none",
+					functions = "none",
+					headings = "bold",
+					operators = "none",
+					strings = "italic",
+					variables = "none",
+
+					-- keywords
+					keywords = "none",
+					keyword_return = "none",
+					keywords_loop = "none",
+					keywords_label = "none",
+					keywords_exception = "none",
+
+					-- builtin
+					builtin_constants = "none",
+					builtin_functions = "none",
+					builtin_types = "none",
+					builtin_variables = "none",
+				},
+				-- Override colors
+				colors = {
+					bg = "#18191a",
+					fg = "#cdcdcd",
+					floatBorder = "#878787",
+					line = "#282830",
+					comment = "#646477",
+					builtin = "#bad1ce",
+					func = "#be8c8c",
+					string = "#deb896",
+					number = "#d2a374",
+					property = "#c7c7d4",
+					constant = "#b4b4ce",
+					parameter = "#b9a3ba",
+					visual = "#363738",
+					error = "#d2788c",
+					warning = "#e6be8c",
+					hint = "#8ca0dc",
+					operator = "#96a3b2",
+					keyword = "#7894ab",
+					type = "#a1b3b9",
+					search = "#465362",
+					plus = "#8faf77",
+					delta = "#e6be8c",
+				},
+			})
+			vim.cmd.colorscheme("vague")
 		end,
 	},
 }
