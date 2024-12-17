4568e055f1b89693af8237cfcbd81a3cb22cbfc8
xavier-castro <xavier_castro@icloud.com>
2024-12-17 14:38:29 -0600

    First setup

---
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+        colorscheme = "base",
+        dim_inactive_windows = true,
+        styles = {
+          italic = false,
+          transparency = true,
+        },
+      })
+    end,
+  },
+
+  {
+    "Mofiqul/vscode.nvim",
+    config = function()
+      require("vscode").setup({
+        transparent = true,
+      })
+    end,
+  },
+}
--- nvim/.config/nvim/lua/xavier/init.lua
+++
@@ -1,7 +0,0 @@
-require 'xavier.set'
-require 'xavier.remap'
-require 'xavier.globals'
-require 'xavier.lazy'
-require 'xavier.autocmds'
-
-
