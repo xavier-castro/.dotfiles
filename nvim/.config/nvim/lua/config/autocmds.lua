9b022ed6e75344606faa4aa06bf95b851dc5b9f4
xavier-castro <xavier_castro@icloud.com>
2024-12-18 04:13:08 -0600

    Add copilot quick toggle

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local copilot_on = true
+vim.api.nvim_create_user_command("CopilotToggle", function()
+  if copilot_on then
+    vim.cmd("Copilot disable")
+    print("Copilot OFF")
+  else
+    vim.cmd("Copilot enable")
+    print("Copilot ON")
+  end
+  copilot_on = not copilot_on
+end, { nargs = 0 })
+vim.keymap.set("", "<M-\\>", ":CopilotToggle<CR>", { noremap = true, silent = true })
