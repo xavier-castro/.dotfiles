fd1f559c1f1fca5a942dcb2d82a749fe0976db25
xavier-castro <xavier_castro@icloud.com>
2024-12-17 23:52:05 -0600

    Add more lazy extras + theme

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -2,8 +2,35 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "vscode",
     },
+  },
+
+  -- Vim Colors Plain
+  {
+    "andreypopp/vim-colors-plain",
+    config = function()
+      -- vim.cmd.colorscheme("plain")
+    end,
+  },
+  -- No Clown Fiesta
+  {
+    "aktersnurra/no-clown-fiesta.nvim",
+    config = function()
+      require("no-clown-fiesta").setup({
+        transparent = true,
+        styles = {
+          -- You can set any of the style values specified for `:h nvim_set_hl`
+          comments = {},
+          keywords = {},
+          functions = {},
+          variables = {},
+          type = { bold = false },
+          lsp = { underline = false },
+        },
+      })
+      -- vim.cmd.colorscheme("no-clown-fiesta")
+    end,
   },
 
   {
