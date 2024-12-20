239afcac678e647efa931fb9171272e9ff3a89a8
xavier-castro <xavier_castro@icloud.com>
2024-12-20 11:02:32 -0600

    Edit cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -14,9 +14,8 @@ return {
       "hrsh7th/cmp-nvim-lua",
       "onsails/lspkind.nvim",
       "rafamadriz/friendly-snippets",
-      "olimorris/codecompanion.nvim",
+      "olimorris/codecompanion.nvim"
     },
-
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
@@ -32,9 +31,13 @@ return {
             local color = string.format("%02x", r) .. string.format("%02x", g) .. string.format("%02x", b)
             local group = "Tw_" .. color
             if vim.fn.hlID(group) < 1 then
-              vim.api.nvim_set_hl(0, group, {
-                fg = "#" .. color,
-              })
+              vim.api.nvim_set_hl(
+                0,
+                group,
+                {
+                  fg = "#" .. color
+                }
+              )
             end
             vim_item.kind = "●"
             vim_item.kind_hl_group = group
@@ -45,97 +48,107 @@ return {
         return vim_item
       end
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<M-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<M-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-f>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.close(),
-          ["<Tab>"] = nil,
-          ["<S-Tab>"] = nil,
-          ["<C-a>"] = cmp.mapping(function(fallback)
-            local suggestion = require("copilot.suggestion")
-            if suggestion.is_visible() then
-              suggestion.accept()
-            elseif cmp.visible() then
-              cmp.confirm({ select = true })
-            else
-              fallback()
+      cmp.setup(
+        {
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
             end
-          end, { "i", "c" }),
-          ["<C-j>"] = function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif require("luasnip").expand_or_jumpable() then
-              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-            else
-              -- local suggestion = require("copilot.suggestion")
-              -- if suggestion.is_visible() then
-              -- 	suggestion.accept()
-              -- local copilot_keys = vim.fn["copilot#Accept"]()
-              -- if copilot_keys ~= "" then
-              -- 	vim.api.nvim_feedkeys(copilot_keys, "i", true)
-              -- else
-              fallback()
-              -- end
-            end
-          end,
-
-          ["<C-k>"] = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif require("luasnip").jumpable(-1) then
-              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-            else
-              fallback()
-            end
-          end,
-        }),
-        sources = {
-          { name = "luasnip", max_item_count = 3 },
-          { name = "nvim_lsp_signature_help" },
-          { name = "path" },
-          {
-            name = "nvim_lsp",
-            max_item_count = 20,
-            entry_filter = entry_filter,
           },
-          { name = "nvim_lua" },
-          { name = "buffer", max_item_count = 8 },
-          { name = "codecompanion" },
-          { name = "neorg" },
-        },
-        window = {
-          documentation = {
-            max_width = 80,
-            max_height = 20,
+          completion = {
+            completeopt = "menu,menuone,noinsert"
           },
-        },
-        performance = {
-          max_view_entries = 64,
-        },
-        formatting = {
-          format = lspkind.cmp_format({
-            maxwidth = 50,
-            before = function(entry, vim_item)
-              vim_item = formatForTailwindCSS(entry, vim_item)
-              return vim_item
-            end,
-          }),
-        },
-      })
-    end,
-  },
+          mapping = cmp.mapping.preset.insert(
+            {
+              ["<M-b>"] = cmp.mapping.scroll_docs(-4),
+              ["<M-f>"] = cmp.mapping.scroll_docs(4),
+              ["<C-f>"] = cmp.mapping.confirm(
+                {
+                  behavior = cmp.ConfirmBehavior.Replace,
+                  select = true
+                }
+              ),
+              ["<C-Space>"] = cmp.mapping.complete(),
+              ["<C-e>"] = cmp.mapping.close(),
+              ["<Tab>"] = nil,
+              ["<S-Tab>"] = nil,
+              ["<C-a>"] = cmp.mapping(
+                function(fallback)
+                  local suggestion = require("copilot.suggestion")
+                  if suggestion.is_visible() then
+                    suggestion.accept()
+                  elseif cmp.visible() then
+                    cmp.confirm({select = true})
+                  else
+                    fallback()
+                  end
+                end,
+                {"i", "c"}
+              ),
+              ["<C-j>"] = function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif require("luasnip").expand_or_jumpable() then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                else
+                  -- end
+                  -- local suggestion = require("copilot.suggestion")
+                  -- if suggestion.is_visible() then
+                  -- 	suggestion.accept()
+                  -- local copilot_keys = vim.fn["copilot#Accept"]()
+                  -- if copilot_keys ~= "" then
+                  -- 	vim.api.nvim_feedkeys(copilot_keys, "i", true)
+                  -- else
+                  fallback()
+                end
+              end,
+              ["<C-k>"] = function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif require("luasnip").jumpable(-1) then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                else
+                  fallback()
+                end
+              end
+            }
+          ),
+          sources = {
+            {name = "luasnip", max_item_count = 3},
+            {name = "nvim_lsp_signature_help"},
+            {name = "path"},
+            {
+              name = "nvim_lsp",
+              max_item_count = 20,
+              entry_filter = entry_filter
+            },
+            {name = "nvim_lua"},
+            {name = "buffer", max_item_count = 8},
+            {name = "codecompanion"},
+            {name = "neorg"}
+          },
+          window = {
+            documentation = {
+              max_width = 80,
+              max_height = 20
+            }
+          },
+          performance = {
+            max_view_entries = 64
+          },
+          formatting = {
+            format = lspkind.cmp_format(
+              {
+                maxwidth = 50,
+                before = function(entry, vim_item)
+                  vim_item = formatForTailwindCSS(entry, vim_item)
+                  return vim_item
+                end
+              }
+            )
+          }
+        }
+      )
+    end
+  }
 }
