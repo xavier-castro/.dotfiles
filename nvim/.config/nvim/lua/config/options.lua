79e2aa6d1cdad3f83c13c53a9075b7dc793732e5
xavier-castro <xavier_castro@icloud.com>
2025-01-05 04:36:25 -0600

    Add my options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,11 @@
 -- Add any additional options here
 -- Disable animations
 vim.g.snacks_animate = false
+
+vim.opt.guicursor = ""
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+vim.opt.wrap = false
