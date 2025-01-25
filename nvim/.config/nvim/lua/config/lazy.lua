b4aab6c87c1df16f75199a43ed35a0e2d1000ef5
xavier-castro <xavier_castro@icloud.com>
2025-01-24 19:18:57 -0600

    Try NVChad

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,11 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    { import = "plugins" }, {import = "plugins.ai"}, {import = "plugins.editor"}, {import = "plugins.lsp"}, {import = "plugins.ui"}
+    -- { import = "plugins" },
+    { import = "plugins.ai" },
+    { import = "plugins.editor" },
+    { import = "plugins.lsp" },
+    { import = "plugins.ui" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
