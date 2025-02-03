ba57fe8fdc932fc6e203a3b2534a8a9255382739
xavier-castro <xavier_castro@icloud.com>
2025-02-03 03:06:55 -0600

    Setup rose-pine

--- nvim/.config/nvim/lua/plugins/ui/colors.lua
+++ nvim/.config/nvim/lua/plugins/ui/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-  color = color or "no-clown-fiesta"
+  color = color or "rose-pine"
   vim.cmd.colorscheme(color)
   -- Set transparent background
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -13,7 +13,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "no-clown-fiesta",
+      colorscheme = "rose-pine",
     },
   },
   {
