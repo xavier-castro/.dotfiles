2276f1eec75f7673658c1fac506248954948d103
xavier-castro <xavier_castro@icloud.com>
2025-02-09 23:18:06 -0600

    Create lazyvim config

---
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-storm",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+      },
+    },
+  },
+}
