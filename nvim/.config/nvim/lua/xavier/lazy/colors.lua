7dd0cb79a33174acbf8b3428766a4d3d4ec33864
xavier-castro <xavier_castro@icloud.com>
2025-02-26 05:59:52 -0600

    Set to VSCode

--- nvim/.config/nvim/lua/xavier/lazy/colors.lua
+++ nvim/.config/nvim/lua/xavier/lazy/colors.lua
@@ -30,27 +30,20 @@ return {
 			require("vscode").setup({
 				-- Alternatively set style in setup
 				-- style = 'light'
-
 				-- Enable transparent background
 				transparent = true,
-
 				-- Enable italic comment
 				italic_comments = true,
-
 				-- Underline `@markup.link.*` variants
 				underline_links = true,
-
 				-- Disable nvim-tree background color
 				disable_nvimtree_bg = true,
-
 				-- Apply theme colors to terminal
 				terminal_colors = true,
-
 				-- Override colors (see ./lua/vscode/colors.lua)
 				-- color_overrides = {
 				-- 	vscLineNumber = "#FFFFFF",
 				-- },
-
 				-- Override highlight groups (see ./lua/vscode/theme.lua)
 				group_overrides = {
 					-- this supports the same val table as vim.api.nvim_set_hl
