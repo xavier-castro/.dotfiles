b6b244353dc81f5e40af39158c862d9dda6a4b07
xavier-castro <xavier_castro@icloud.com>
2025-02-25 20:35:22 -0600

    Add autopairs vim-maximizer git-worktree and conform edits

--- nvim/.config/nvim/lua/xavier/lazy/lsp.lua
+++ nvim/.config/nvim/lua/xavier/lazy/lsp.lua
@@ -15,20 +15,89 @@ return {
 	},
 
 	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				typescript = { "prettierd" },
-				typescriptreact = { "prettierd" },
-				javascript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				html = { "prettierd" },
-				css = { "prettierd" },
-				json = { "prettierd" },
-				markdown = { "prettierd" },
-				python = { "black" },
+		local conform = require("conform")
+
+		conform.setup({
+			formatters = {
+				["markdown-toc"] = {
+					condition = function(_, ctx)
+						for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+							if line:find("<!%-%- toc %-%->") then
+								return true
+							end
+						end
+					end,
+				},
+				["markdownlint-cli2"] = {
+					condition = function(_, ctx)
+						local diag = vim.tbl_filter(function(d)
+							return d.source == "markdownlint"
+						end, vim.diagnostic.get(ctx.buf))
+						return #diag > 0
+					end,
+				},
 			},
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				-- markdown = { "prettier" },
+				graphql = { "prettier" },
+				liquid = { "prettier" },
+				lua = { "stylua" },
+				python = { "black" },
+				markdown = { "prettier" },
+				["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+			},
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 1000,
+			-- },
 		})
+
+		-- Configure individual formatters
+		conform.formatters.prettier = {
+			args = {
+				"--stdin-filepath",
+				"$FILENAME",
+				"--tab-width",
+				"4",
+				"--use-tabs",
+				"false",
+			},
+		}
+		conform.formatters.shfmt = {
+			prepend_args = { "-i", "4" },
+		}
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = " Prettier Format whole file or range (in visual mode) with" })
+		-- require("conform").setup({
+		-- 	formatters_by_ft = {
+		-- 		lua = { "stylua" },
+		-- 		typescript = { "prettierd" },
+		-- 		typescriptreact = { "prettierd" },
+		-- 		javascript = { "prettierd" },
+		-- 		javascriptreact = { "prettierd" },
+		-- 		html = { "prettierd" },
+		-- 		css = { "prettierd" },
+		-- 		json = { "prettierd" },
+		-- 		markdown = { "prettierd" },
+		-- 		python = { "black" },
+		-- 	},
+		-- })
 		vim.api.nvim_create_user_command("Format", function(args)
 			local range = nil
 			if args.count ~= -1 then
