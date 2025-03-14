8eb94e3847571c765fc6cd559ef4a9c8cc18f25a
xavier-castro <xavier_castro@icloud.com>
2025-03-14 00:29:33 -0500

    Stop touching editor

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -21,7 +21,7 @@ return {
       -- Lua
       require("onedark").setup({
         -- Main options --
-        style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        style = "cool", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
         transparent = true, -- Show/hide background
         term_colors = true, -- Change terminal color as per the selected theme style
         ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
--- nvim/.config/nvim/lua/plugins/lazyvim.lua
+++ nvim/.config/nvim/lua/plugins/lazyvim.lua
@@ -1,6 +1,6 @@
 return {
   "LazyVim/LazyVim",
   opts = {
-    colorscheme = "onedark",
+    colorscheme = "solarized-osaka",
   },
 }
