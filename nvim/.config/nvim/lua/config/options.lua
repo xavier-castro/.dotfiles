4d76a7e747f8251cf343acf6b396c3052979022a
xavier-castro <xavier_castro@icloud.com>
2025-03-13 20:41:12 -0500

    Edit options.lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,58 +5,13 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.opt.guicursor = ""
--- vim.opt.nu = true
--- vim.opt.relativenumber = true
--- vim.opt.tabstop = 2
--- vim.opt.softtabstop = 2
--- vim.opt.shiftwidth = 2
--- vim.opt.expandtab = true
--- vim.opt.smartindent = true
--- vim.opt.wrap = false
--- vim.opt.swapfile = false
--- vim.opt.backup = false
--- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
--- vim.opt.undofile = true
--- vim.opt.hlsearch = false
--- vim.opt.incsearch = true
--- vim.opt.termguicolors = true
--- vim.opt.scrolloff = 8
--- vim.opt.signcolumn = "yes"
--- vim.opt.isfname:append("@-@")
--- vim.opt.updatetime = 50
--- vim.opt.colorcolumn = "80"
-
--- NOTE: LazyVim Overrides:
--- This file is automatically loaded by plugins.core
--- LazyVim auto format
--- vim.g.autoformat = true
-
--- Snacks animations
--- Set to `false` to globally disable all snacks animations
-vim.g.snacks_animate = true
---
--- -- LazyVim picker to use.
--- -- Can be one of: telescope, fzf
--- -- Leave it to "auto" to automatically use the picker
--- -- enabled with `:LazyExtras`
--- vim.g.lazyvim_picker = "telescope"
---
--- -- LazyVim completion engine to use.
--- -- Can be one of: nvim-cmp, blink.cmp
--- -- Leave it to "auto" to automatically use the completion engine
--- -- enabled with `:LazyExtras`
--- vim.g.lazyvim_cmp = "blink"
-
--- if the completion engine supports the AI source,
--- use that instead of inline suggestions
-vim.g.ai_cmp = false
-vim.g.snacks_animate = false
 
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 
 vim.opt.number = true
-
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
@@ -105,4 +60,30 @@ vim.filetype.add({
 
 vim.g.lazyvim_prettier_needs_config = true
 vim.g.lazyvim_picker = "telescope"
+
+-- NOTE: LazyVim Overrides:
+-- This file is automatically loaded by plugins.core
+-- LazyVim auto format
+-- vim.g.autoformat = true
+
+-- Snacks animations
+-- Set to `false` to globally disable all snacks animations
+vim.g.snacks_animate = true
+--
+-- -- LazyVim picker to use.
+-- -- Can be one of: telescope, fzf
+-- -- Leave it to "auto" to automatically use the picker
+-- -- enabled with `:LazyExtras`
+-- vim.g.lazyvim_picker = "telescope"
+--
+-- -- LazyVim completion engine to use.
+-- -- Can be one of: nvim-cmp, blink.cmp
+-- -- Leave it to "auto" to automatically use the completion engine
+-- -- enabled with `:LazyExtras`
+-- vim.g.lazyvim_cmp = "blink"
+
+-- if the completion engine supports the AI source,
+-- use that instead of inline suggestions
+vim.g.ai_cmp = false
+vim.g.snacks_animate = false
 vim.g.lazyvim_cmp = "blink.cmp"
