b35ca9983c87e1b2273459ff83fe6051306cf7db
xavier-castro <xavier_castro@icloud.com>
2025-03-14 00:04:49 -0500

    Fix c-c issue

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,11 +1,12 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    -- keymap = {
-    --   preset = "enter",
-    --   ["<C-c>"] = { "hide" },
-    --   ["<C-k>"] = { "snippet_forward", "fallback" },
-    --   ["<C-j>"] = { "snippet_backward", "fallback" },
+    keymap = {
+      preset = "enter",
+      ["<C-k>"] = { "snippet_forward", "fallback" },
+      ["<C-j>"] = { "snippet_backward", "fallback" },
+      ["<CR>"] = {},
+    },
     completion = {
       menu = {
         winblend = vim.o.pumblend,
