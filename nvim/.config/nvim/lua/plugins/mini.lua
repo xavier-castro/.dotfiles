9bc576df4e98199b30c74951366699f887da27fd
xavier-castro <xavier_castro@icloud.com>
2025-03-17 13:10:38 -0500

    Add leap

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,18 @@
+return { -- Collection of various small independent pluins/modules
+  "echasnovski/mini.nvim",
+  config = function()
+    -- -- Configure Files with minimal UI
+    require("mini.files").setup({
+      -- TODO: Set preview to true when done with project to not accidentally review important files
+      windows = {
+        preview = true,
+      },
+    })
+    vim.keymap.set(
+      "n",
+      "<leader>E",
+      ":lua MiniFiles.open()<CR>",
+      { noremap = true, silent = true, desc = "MiniFile [E]xplorer" }
+    )
+  end,
+}
--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -17,7 +17,7 @@ return {
       desc = "Explorer NeoTree (cwd)",
     },
     -- { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
-    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+    -- { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
     {
       "<leader>ge",
       function()
