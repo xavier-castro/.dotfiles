6a9a1a51503f3394a60d45562a633b56d0b4d677
xavier-castro <xavier_castro@icloud.com>
2025-03-17 11:56:39 -0500

    Update dashboard

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -3,5 +3,15 @@ return {
   opts = {
     explorer = { enabled = false },
     picker = { enabled = false },
+    indent = {
+      enabled = false,
+    },
+    dashboard = {
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+      },
+    },
   },
 }
