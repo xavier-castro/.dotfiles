7b9c928e002750b1a116aaa203cf98589f5543fb
xavier-castro <xavier_castro@icloud.com>
2025-03-18 16:15:07 -0500

    Change

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,95 @@
+return {
+  {
+    "folke/snacks.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function(_, opts)
+      ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+      local progress = vim.defaulttable()
+      vim.api.nvim_create_autocmd("LspProgress", {
+        ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+        callback = function(ev)
+          local client = vim.lsp.get_client_by_id(ev.data.client_id)
+          local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+          if not client or type(value) ~= "table" then
+            return
+          end
+          local p = progress[client.id]
+
+          for i = 1, #p + 1 do
+            if i == #p + 1 or p[i].token == ev.data.params.token then
+              p[i] = {
+                token = ev.data.params.token,
+                msg = ("[%3d%%] %s%s"):format(
+                  value.kind == "end" and 100 or value.percentage or 100,
+                  value.title or "",
+                  value.message and (" **%s**"):format(value.message) or ""
+                ),
+                done = value.kind == "end",
+              }
+              break
+            end
+          end
+
+          local msg = {} ---@type string[]
+          progress[client.id] = vim.tbl_filter(function(v)
+            return table.insert(msg, v.msg) or not v.done
+          end, p)
+
+          local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+          vim.notify(table.concat(msg, "\n"), "info", {
+            id = "lsp_progress",
+            title = client.name,
+            opts = function(notif)
+              notif.icon = #progress[client.id] == 0 and " "
+                or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+          })
+        end,
+      })
+
+      opts.bigfile = { enabled = true }
+      opts.indent = { enabled = false }
+      opts.words = { enabled = true }
+      opts.statuscolumn = { enabled = false }
+      opts.notifier = { enabled = true, timeout = 4000, top_down = true, style = "compact" }
+      opts.styles = { lazygit = { width = 0, height = 0 } }
+      opts.styles["input"] = {
+        relative = "cursor",
+        row = -3,
+        col = 0,
+        width = 40,
+        keys = { i_esc = { "<esc>", "stopinsert", mode = "i" } },
+      }
+      opts.picker = { enabled = false }
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.views = {
+        cmdline_popup = { position = { row = 35, col = "50%" } },
+        cmdline_popupmenu = { position = { row = 38, col = "50%" } },
+        mini = { win_options = { winblend = 0 } },
+      }
+      opts.lsp = { progress = { enabled = false }, hover = { silent = true } }
+      opts.presets.lsp_doc_border = true
+
+      -- Suppresing notifications
+      opts.routes = {
+        { filter = { event = "msg_show", find = "written" } },
+        { filter = { event = "msg_show", find = "yanked" } },
+        { filter = { event = "msg_show", find = "%d+L, %d+B" } },
+        { filter = { event = "msg_show", find = "; after #%d+" } },
+        { filter = { event = "msg_show", find = "; before #%d+" } },
+        { filter = { event = "msg_show", find = "%d fewer lines" } },
+        { filter = { event = "msg_show", find = "%d more lines" } },
+        { filter = { event = "msg_show", find = "%d lines indented" } },
+        { filter = { event = "msg_show", find = "%d lines moved" } },
+        { filter = { event = "msg_show", find = "<ed" } },
+        { filter = { event = "msg_show", find = ">ed" } },
+        { filter = { event = "lsp", kind = "progress", find = "jdtls" } },
+      }
+    end,
+  },
+}
