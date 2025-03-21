a7e1139b9f7c7e455e012f1b2165e84fcec2e476
xavier-castro <xavier_castro@icloud.com>
2025-03-21 01:50:43 -0500

    Edit conform

--- nvim/.config/nvim/lua/kickstart/plugins/conform.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/conform.lua
@@ -33,9 +33,13 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
+        -- TypeScript
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        -- Markdown
+        markdown = { 'prettier' },
+        -- Python
+        python = { 'isort', 'black' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
