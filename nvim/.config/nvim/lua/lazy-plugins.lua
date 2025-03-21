9f2f3a572c9744da11db2e9582eb87559a86b4f3
xavier-castro <xavier_castro@icloud.com>
2025-03-21 08:59:48 -0500

    Actually setup config your way

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -23,27 +23,35 @@ require('lazy').setup({
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart/plugins/gitsigns',
+  require 'kickstart.plugins.gitsigns',
 
-  require 'kickstart/plugins/which-key',
+  require 'kickstart.plugins.which-key',
 
-  require 'kickstart/plugins/telescope',
+  require 'kickstart.plugins.oil',
 
-  require 'kickstart/plugins/lspconfig',
+  require 'kickstart.plugins.telescope',
 
-  require 'kickstart/plugins/conform',
+  require 'kickstart.plugins.lspconfig',
 
-  require 'kickstart/plugins/cmp',
+  require 'kickstart.plugins.conform',
+
+  require 'kickstart.plugins.cmp',
+
+  require 'kickstart.plugins.copilot',
 
   require 'kickstart/plugins/tokyonight',
 
   require 'kickstart/plugins/todo-comments',
 
-  require 'kickstart/plugins/mini',
+  require 'kickstart.plugins.mini',
 
-  require 'kickstart/plugins/treesitter',
+  require 'kickstart.plugins.treesitter',
 
   require 'kickstart.plugins.avante',
+
+  require 'kickstart.plugins.autopairs',
+
+  require 'kickstart.plugins.tabout',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
