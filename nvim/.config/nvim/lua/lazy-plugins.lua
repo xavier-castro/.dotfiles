b5401eaff475479bf252585c1f5258340cd66d2c
xavier-castro <xavier_castro@icloud.com>
2025-03-25 09:01:12 -0500

    Add trouble and undotree

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -44,6 +44,9 @@ require('lazy').setup({
 
   -- NOTE: Xavier plugins (testing / Seeing if they belong in core)
 
+  require 'xavier.plugins.undotree',
+  require 'xavier.plugins.trouble',
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
