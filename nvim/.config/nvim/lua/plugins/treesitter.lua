e159a58cc3becc26b620edf5ea300cf78f7793d1
xavier-castro <xavier_castro@icloud.com>
2025-03-25 11:12:31 -0500

    Edit treesitter and dashboard

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "astro",
+      "cmake",
+      "cpp",
+      "gitignore",
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+  },
+}
