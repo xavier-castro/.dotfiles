f7fbe63fa4bb863fb5ce8501e6795e9ccb3daba9
xavier-castro <xavier_castro@icloud.com>
2025-03-29 04:38:02 -0500

    Scratch

--- nvim/.config/nvim/README.md
+++ nvim/.config/nvim/README.md
@@ -1,3 +1,4 @@
-# XVVR Neovim Config
+# 💤 LazyVim
 
-## LazyVim Defaults with Minor Adjustments
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,25 +1,10 @@
 return {
   "saghen/blink.cmp",
-  dependencies = {
-    "Kaiser-Yang/blink-cmp-avante",
-    -- ... Other dependencies
-  },
   opts = {
     keymap = {
-      ["<CR>"] = {}, -- Disables <CR>
-    },
-    sources = {
-      -- Add 'avante' to the list
-      default = { "avante" },
-      providers = {
-        avante = {
-          module = "blink-cmp-avante",
-          name = "Avante",
-          opts = {
-            -- options for blink-cmp-avante
-          },
-        },
-      },
+      preset = "enter",
+      ["<CR>"] = {},
+      ["<Tab>"] = {},
     },
   },
 }
