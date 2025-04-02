730082e370b433b52e7001b5e44cfe8c9ff4844d
xavier-castro <xavier_castro@icloud.com>
2025-04-02 10:13:44 -0500

    Add a lot

--- nvim/.config/nvim/lua/xavier/lazy/aider.lua
+++ nvim/.config/nvim/lua/xavier/lazy/aider.lua
@@ -1,39 +1,68 @@
 return {
-    "GeorgesAlkhouri/nvim-aider",
-    cmd = {
-      "AiderTerminalToggle", "AiderHealth",
+  "GeorgesAlkhouri/nvim-aider",
+  event = "BufEnter",
+  lazy = false,
+  cmd = {
+    "AiderTerminalToggle", "AiderHealth",
+  },
+  keys = {
+    { "<leader>a/", "<cmd>AiderTerminalToggle<cr>",    desc = "Open Aider" },
+    { "<leader>as", "<cmd>AiderTerminalSend<cr>",      desc = "Send to Aider",                  mode = { "n", "v" } },
+    { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>",  desc = "Send Command To Aider" },
+    { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>",   desc = "Send Buffer To Aider" },
+    { "<leader>a+", "<cmd>AiderQuickAddFile<cr>",      desc = "Add File to Aider" },
+    { "<leader>a-", "<cmd>AiderQuickDropFile<cr>",     desc = "Drop File from Aider" },
+    { "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
+    -- Example nvim-tree.lua integration if needed
+    { "<leader>a+", "<cmd>AiderTreeAddFile<cr>",       desc = "Add File from Tree to Aider",    ft = "NvimTree" },
+    { "<leader>a-", "<cmd>AiderTreeDropFile<cr>",      desc = "Drop File from Tree from Aider", ft = "NvimTree" },
+  },
+  dependencies = {
+    "folke/snacks.nvim",
+    --- The below dependencies are optional
+    "catppuccin/nvim",
+    "nvim-tree/nvim-tree.lua",
+    "nvim-neo-tree/neo-tree.nvim",
+    {
+      "MunifTanjim/nui.nvim",
+      name = "nui"
     },
-    keys = {
-      { "<leader>a/", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
-      { "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
-      { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
-      { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
-      { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
-      { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
-      { "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
-      -- Example nvim-tree.lua integration if needed
-      { "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
-      { "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
+    --- Neo-tree integration
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      config = function()
+        require("neo-tree").setup({
+          sources = { "filesystem" },
+
+          enable_diagnostics = false,
+          enable_git_status = false,
+
+          filesystem = {
+            hijack_netrw_behavior = "disabled",
+            bind_to_cwd = false,
+            follow_current_file = { enabled = true },
+          },
+
+          window = {
+            mappings = {
+              ["<space>"] = "none",
+            },
+          },
+
+          default_component_configs = {
+            name = {
+              trailing_slash = true,
+              use_git_status_colors = false,
+            },
+            indent = {
+              with_expanders = true,
+            },
+          },
+        })
+
+      end
+
     },
-    dependencies = {
-      "folke/snacks.nvim",
-      --- The below dependencies are optional
-      "catppuccin/nvim",
-      "nvim-tree/nvim-tree.lua",
-      --- Neo-tree integration
-      {
-        "nvim-neo-tree/neo-tree.nvim",
-        opts = function(_, opts)
-          -- Example mapping configuration (already set by default)
-          -- opts.window = {
-          --   mappings = {
-          --     ["+"] = { "nvim_aider_add", desc = "add to aider" },
-          --     ["-"] = { "nvim_aider_drop", desc = "drop from aider" }
-          --   }
-          -- }
-          require("nvim_aider.neo_tree").setup(opts)
-        end,
-      },
-    },
-    config = true,
-  }
+  },
+  config = true,
+}
