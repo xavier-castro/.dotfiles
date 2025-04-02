4516873cb255103435610966632c9bcd30d4bd20
xavier-castro <xavier_castro@icloud.com>
2025-04-02 03:54:02 -0500

    Edit lspsaga

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -7,7 +7,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 		vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
 		vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<cr>", opts)
-		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		-- vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gD", "<cmd>Lspsaga goto_definition<cr>", opts)
 		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
 		vim.keymap.set("n", "go", "<cmd>Lspsaga peek_type_definition<cr>", opts)
 		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
