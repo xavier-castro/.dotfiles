590882b2c6495a54ad24848b1ae99c71e0927687
xavier-castro <xavier_castro@icloud.com>
2025-04-03 19:26:29 -0500

    Change aider args

--- nvim/.config/nvim/lua/xavier/lazy/aider.lua
+++ nvim/.config/nvim/lua/xavier/lazy/aider.lua
@@ -6,21 +6,20 @@ return {
     "AiderTerminalToggle", "AiderHealth",
   },
   keys = {
-    { "<leader>a/", "<cmd>AiderTerminalToggle<cr>",    desc = "Open Aider" },
-    { "<leader>as", "<cmd>AiderTerminalSend<cr>",      desc = "Send to Aider",                  mode = { "n", "v" } },
-    { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>",  desc = "Send Command To Aider" },
-    { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>",   desc = "Send Buffer To Aider" },
-    { "<leader><leader><cr>",   "<cmd>AiderQuickSendBuffer<cr>",   desc = "Send Buffer to Aider" },
-    { "<leader>a+", "<cmd>AiderQuickAddFile<cr>",      desc = "Add File to Aider" },
-    { "<leader>a-", "<cmd>AiderQuickDropFile<cr>",     desc = "Drop File from Aider" },
-    { "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
+    { "<leader>a/",           "<cmd>AiderTerminalToggle<cr>",    desc = "Open Aider" },
+    { "<leader>as",           "<cmd>AiderTerminalSend<cr>",      desc = "Send to Aider",                  mode = { "n", "v" } },
+    { "<leader>ac",           "<cmd>AiderQuickSendCommand<cr>",  desc = "Send Command To Aider" },
+    { "<leader>ab",           "<cmd>AiderQuickSendBuffer<cr>",   desc = "Send Buffer To Aider" },
+    { "<leader><leader><cr>", "<cmd>AiderQuickSendBuffer<cr>",   desc = "Send Buffer to Aider" },
+    { "<leader>a+",           "<cmd>AiderQuickAddFile<cr>",      desc = "Add File to Aider" },
+    { "<leader>a-",           "<cmd>AiderQuickDropFile<cr>",     desc = "Drop File from Aider" },
+    { "<leader>ar",           "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
     -- Example nvim-tree.lua integration if needed
-    { "<leader>a+", "<cmd>AiderTreeAddFile<cr>",       desc = "Add File from Tree to Aider",    ft = "NvimTree" },
-    { "<leader>a-", "<cmd>AiderTreeDropFile<cr>",      desc = "Drop File from Tree from Aider", ft = "NvimTree" },
+    { "<leader>a+",           "<cmd>AiderTreeAddFile<cr>",       desc = "Add File from Tree to Aider",    ft = "NvimTree" },
+    { "<leader>a-",           "<cmd>AiderTreeDropFile<cr>",      desc = "Drop File from Tree from Aider", ft = "NvimTree" },
   },
   dependencies = {
     "folke/snacks.nvim",
-    --- The below dependencies are optional
     "catppuccin/nvim",
     "nvim-tree/nvim-tree.lua",
     "nvim-neo-tree/neo-tree.nvim",
@@ -65,4 +64,15 @@ return {
     },
   },
   config = true,
+  opts = {
+    aider_cmd = "aider",
+    -- Command line arguments passed to aider
+    args = {
+      "--no-auto-commits",
+      "--pretty",
+      "--stream",
+      "--model gemini/gemini-2.5-pro-exp-03-25",
+    },
+
+  }
 }
