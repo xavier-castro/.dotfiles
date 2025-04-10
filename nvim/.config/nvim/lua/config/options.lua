a4f65871a6dab7c1ff943e44b7776c0f3c360348
xavier-castro <xavier_castro@icloud.com>
2025-04-10 01:53:57 -0500

    Add a lot

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,96 +1,46 @@
--- Set the background theme to dark
-vim.opt.background = "dark"
-
--- Disable backup file creation
-vim.opt.backup = false
-
--- Set the command line height to 1 line
-vim.opt.cmdheight = 1
-
--- Use spaces instead of tabs
-vim.opt.expandtab = true
-
--- Support multiple file formats
-vim.opt.fileformats = { "unix", "mac", "dos" }
-
--- Disable code folding by default
-vim.opt.foldenable = false
-
--- Allow buffer switching without saving
-vim.opt.hidden = true
-
--- Set command history to remember 1000 commands
-vim.opt.history = 1000
-
--- Ignore case in search patterns
-vim.opt.ignorecase = true
-
--- Enable lazy redraw for faster macros and scripts
-vim.opt.lazyredraw = true
-
--- Enable regular expression magic
-vim.opt.magic = true
-
--- Display line numbers
-vim.opt.number = true
-
--- Display relative line numbers
-vim.opt.relativenumber = true
-
--- Number of spaces to use for each step of (auto)indent
-vim.opt.shiftwidth = 2
-
--- Disable highlighting matching parentheses
-vim.opt.showmatch = false
-
--- Display the sign column with line numbers
-vim.opt.signcolumn = "yes"
-
--- Override ignorecase if search pattern contains uppercase letters
-vim.opt.smartcase = true
-
--- Number of spaces that a <Tab> counts for while performing editing operations
-vim.opt.softtabstop = 2
-
--- Disable swap file creation
-vim.opt.swapfile = false
-
--- Number of spaces that a <Tab> in the file counts for
-vim.opt.tabstop = 2
-
--- Enable true color support
-vim.opt.termguicolors = true
-
--- Allow specified keys to move to the previous/next line when at the beginning/end of a line
-vim.opt.whichwrap = vim.opt.whichwrap:append("<,>,[,]")
-
--- Enable command-line completion mode
-vim.opt.wildmenu = true
-
--- Disable line wrapping
-vim.opt.wrap = false
-
--- Disable write backup file creation
-vim.opt.writebackup = false
-
--- Highlight current cursor line
-vim.opt.cursorline = true
-
--- Enable system clipboard integration
-vim.opt.clipboard:append("unnamedplus")
-
--- Set title string
-vim.opt.title = true
-vim.opt.titlestring = [[%{fnamemodify(getcwd(),':t')} - %{expand('%:.')}]]
-
--- Ensure the completion menu appears but does not automatically select any item
-vim.opt.completeopt:append("noselect")
-
--- Stop the terminal's cursor from blinking
 vim.opt.guicursor = ""
+vim.opt.cursorline = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 80
+vim.opt.colorcolumn = "80"
+vim.opt.showmode = false
 
--- Change the terminal's cursor
-vim.opt.guicursor = {
-  "n-v-c:block", -- Block in normal mode
-  "i:ver25",     -- Vertical line in insert mode
-}
+
+-- Nice and simple folding:
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = ""
+vim.opt.foldcolumn = "0"
+vim.opt.fillchars:append({ fold = " " })
+vim.o.foldmethod = 'expr'
+-- Default to treesitter folding
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client:supports_method('textDocument/foldingRange') then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
--- nvim/.config/nvim/lua/plugins/grug-far.lua
+++ nvim/.config/nvim/lua/plugins/grug-far.lua
@@ -1,14 +1,22 @@
-return  {
-    'MagicDuck/grug-far.nvim',
-    config = function()
-      -- optional setup call to override plugin options
-      -- alternatively you can set options with vim.g.grug_far = { ... }
-      require('grug-far').setup({
-        -- options, see Configuration section below
-        -- there are no required options atm
-        -- engine = 'ripgrep' is default, but 'astgrep' or 'astgrep-rules' can
-        -- be specified
-      });
-    end
-  }
-
+return {
+  "MagicDuck/grug-far.nvim",
+  opts = { headerMaxWidth = 80 },
+  cmd = "GrugFar",
+  keys = {
+    {
+      "<leader>sr",
+      function()
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.open({
+          transient = true,
+          prefills = {
+            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+          },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace",
+    },
+  },
+}
