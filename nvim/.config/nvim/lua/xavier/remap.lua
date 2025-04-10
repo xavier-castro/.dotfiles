cbc07d0f73a3f031a8594ca03e07ed2af0f5c638
xavier-castro <xavier_castro@icloud.com>
2025-04-10 08:46:26 -0500

    Add grug and float

--- nvim/.config/nvim/lua/xavier/remap.lua
+++ nvim/.config/nvim/lua/xavier/remap.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "-", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -71,3 +72,5 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end
 	end,
 })
+
+require("xavier.utils.float-terminal")
