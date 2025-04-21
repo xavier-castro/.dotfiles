c1ad314e0f0f2610a73c9323432f6a1bd3274bfd
xavier-castro <xavier_castro@icloud.com>
2025-04-21 09:59:49 -0500

    Remove bufferline

--- nvim/.config/nvim/lua/xavier/lazy/zenmode.lua
+++ nvim/.config/nvim/lua/xavier/lazy/zenmode.lua
@@ -1,37 +1,12 @@
-
 return {
-    "folke/zen-mode.nvim",
-    config = function()
-        vim.keymap.set("n", "<leader>zz", function()
-            require("zen-mode").setup {
-                window = {
-                    width = 90,
-                    options = { }
-                },
-            }
-            require("zen-mode").toggle()
-            vim.wo.wrap = false
-            vim.wo.number = true
-            vim.wo.rnu = true
-            ColorMyPencils()
-        end)
-
-
-        vim.keymap.set("n", "<leader>zZ", function()
-            require("zen-mode").setup {
-                window = {
-                    width = 80,
-                    options = { }
-                },
-            }
-            require("zen-mode").toggle()
-            vim.wo.wrap = false
-            vim.wo.number = false
-            vim.wo.rnu = false
-            vim.opt.colorcolumn = "0"
-            ColorMyPencils()
-        end)
-    end
+	"folke/zen-mode.nvim",
+	cmd = "ZenMode",
+	opts = {
+		plugins = {
+			gitsigns = true,
+			tmux = true,
+			kitty = { enabled = false, font = "+2" },
+		},
+	},
+	keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
 }
-
-
