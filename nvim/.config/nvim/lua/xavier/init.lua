88b70e0fdabb2edb22dab49c7b8e5ef5f55519e5
xavier-castro <xavier_castro@icloud.com>
2025-04-21 09:10:54 -0500

    Edit and move

--- nvim/.config/nvim/lua/xavier/init.lua
+++ nvim/.config/nvim/lua/xavier/init.lua
@@ -8,75 +8,93 @@ local lsp_path = vim.fn.stdpath("config") .. "/lua/config/lsp"
 -- First, explicitly load the global configuration
 require("config.lsp.global")
 
-
 -- Then load all other LSP configs
 for _, file in ipairs(vim.fn.readdir(lsp_path)) do
-  if file:match("%.lua$") and file ~= "global.lua" then
-    local module_name = "config.lsp." .. file:gsub("%.lua$", "")
-    require(module_name)
-  end
+	if file:match("%.lua$") and file ~= "global.lua" then
+		local module_name = "config.lsp." .. file:gsub("%.lua$", "")
+		require(module_name)
+	end
 end
 
 local augroup = vim.api.nvim_create_augroup
-local XavierGroup = augroup('Xavier', {})
+local XavierGroup = augroup("Xavier", {})
 
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+local yank_group = augroup("HighlightYank", {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+	require("plenary.reload").reload_module(name)
 end
 
 vim.filetype.add({
-    extension = {
-        templ = 'templ',
-    }
+	extension = {
+		templ = "templ",
+	},
 })
 
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
 })
 
 autocmd({ "BufWritePre" }, {
-    group = XavierGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+	group = XavierGroup,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
 
-autocmd('BufEnter', {
-    group = XavierGroup,
-    callback = function()
-        if vim.bo.filetype == "zig" then
-            vim.cmd.colorscheme("tokyonight-night")
-        else
-            vim.cmd.colorscheme("rose-pine-moon")
-        end
-    end
+autocmd("BufEnter", {
+	group = XavierGroup,
+	callback = function()
+		if vim.bo.filetype == "zig" then
+			vim.cmd.colorscheme("tokyonight-night")
+		else
+			vim.cmd.colorscheme("rose-pine-moon")
+		end
+	end,
 })
 
-
-autocmd('LspAttach', {
-    group = XavierGroup,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    end
+autocmd("LspAttach", {
+	group = XavierGroup,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, opts)
+		vim.keymap.set("n", "K", function()
+			vim.lsp.buf.hover()
+		end, opts)
+		vim.keymap.set("n", "<leader>vws", function()
+			vim.lsp.buf.workspace_symbol()
+		end, opts)
+		vim.keymap.set("n", "<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, opts)
+		vim.keymap.set("n", "<leader>vca", function()
+			vim.lsp.buf.code_action()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrr", function()
+			vim.lsp.buf.references()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+		vim.keymap.set("n", "[d", function()
+			vim.diagnostic.jump({ count = 1, float = true })
+		end, opts)
+		vim.keymap.set("n", "]d", function()
+			vim.diagnostic.jump({ count = -1, float = true })
+		end, opts)
+	end,
 })
 
 local save_fold = vim.api.nvim_create_augroup("save_fold", {})
@@ -98,10 +116,10 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 })
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = function()
-    vim.cmd [[highlight CursorLine guibg=#383a4a ctermbg=290]]
-  end
+	pattern = "*",
+	callback = function()
+		vim.cmd([[highlight CursorLine guibg=#383a4a ctermbg=290]])
+	end,
 })
 
 vim.g.netrw_browse_split = 0
