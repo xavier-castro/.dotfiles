fec2aab508059ac16ebf87dcfe7d18fe9fc23a3c
xavier-castro <xavier_castro@icloud.com>
2025-04-29 15:02:07 -0500

    Change oil bind

--- nvim/.config/nvim/lua/xavier/lazy/oil.lua
+++ nvim/.config/nvim/lua/xavier/lazy/oil.lua
@@ -27,6 +27,6 @@ return {
     config = function(_, opts)
         local oil = require("oil")
         oil.setup(opts)
-        vim.keymap.set("n", "-", oil.open, {})
+        vim.keymap.set("n", "<leader>pv", oil.open, {})
     end,
 }
