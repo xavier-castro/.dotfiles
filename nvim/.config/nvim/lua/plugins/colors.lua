f67b544f3db222914a58c552a23d098ea6113951
xavier-castro <xavier_castro@icloud.com>
2025-04-29 16:58:48 -0500

    Change to rose-pine-moon

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-  color = color or "rose-pine"
+  color = color or "rose-pine-moon"
   vim.cmd.colorscheme(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -12,44 +12,53 @@ return {
     name = "rose-pine",
     priority = 1000,
     opts = {
-      dark_variant = "moon",
-      styles = { italic = false },
-      palette = {
-        dawn = {
-          no_bg = "#faf4ed",
-          cursor_bg = "#000000",
-          cursor_fg = "#ffffff",
-        },
-        moon = {
-          gold = "#f6d5a7",
-          foam = "#a1d1da",
-          iris = "#d9c7ef",
-          rose = "#ebbcba",
-          pine = "#437e91",
-          no_bg = "#000000",
-          cursor_bg = "#ffffff",
-          cursor_fg = "#000000",
-        },
-      },
-      highlight_groups = {
-        Normal = { bg = "no_bg" },
-        Cursor = { bg = "cursor_bg", fg = "cursor_fg" },
-        Directory = { fg = "foam", bold = false },
-        StatusLine = { bg = "surface", fg = "subtle" },
-        StatusLineTerm = { link = "StatusLine" },
-        StatusLineNC = { link = "StatusLine" },
-        --- gitsigns
-        StatusLineGitSignsAdd = { bg = "surface", fg = "pine" },
-        StatusLineGitSignsChange = { bg = "surface", fg = "gold" },
-        StatusLineGitSignsDelete = { bg = "surface", fg = "rose" },
-        --- diagnostics
-        StatusLineDiagnosticSignError = { bg = "surface", fg = "love" },
-        StatusLineDiagnosticSignWarn = { bg = "surface", fg = "gold" },
-        StatusLineDiagnosticSignInfo = { bg = "surface", fg = "foam" },
-        StatusLineDiagnosticSignHint = { bg = "surface", fg = "iris" },
-        StatusLineDiagnosticSignOk = { bg = "surface", fg = "pine" },
+      styles = {
+        transparency = true,
+        italic = false,
       },
     },
+    config = function()
+      ColorMyPencils()
+    end,
+    -- opts = {
+    --   dark_variant = "moon",
+    --   styles = { italic = false },
+    --   palette = {
+    --     dawn = {
+    --       no_bg = "#faf4ed",
+    --       cursor_bg = "#000000",
+    --       cursor_fg = "#ffffff",
+    --     },
+    --     moon = {
+    --       gold = "#f6d5a7",
+    --       foam = "#a1d1da",
+    --       iris = "#d9c7ef",
+    --       rose = "#ebbcba",
+    --       pine = "#437e91",
+    --       no_bg = "#000000",
+    --       cursor_bg = "#ffffff",
+    --       cursor_fg = "#000000",
+    --     },
+    --   },
+    --   highlight_groups = {
+    --     Normal = { bg = "no_bg" },
+    --     Cursor = { bg = "cursor_bg", fg = "cursor_fg" },
+    --     Directory = { fg = "foam", bold = false },
+    --     StatusLine = { bg = "surface", fg = "subtle" },
+    --     StatusLineTerm = { link = "StatusLine" },
+    --     StatusLineNC = { link = "StatusLine" },
+    --     --- gitsigns
+    --     StatusLineGitSignsAdd = { bg = "surface", fg = "pine" },
+    --     StatusLineGitSignsChange = { bg = "surface", fg = "gold" },
+    --     StatusLineGitSignsDelete = { bg = "surface", fg = "rose" },
+    --     --- diagnostics
+    --     StatusLineDiagnosticSignError = { bg = "surface", fg = "love" },
+    --     StatusLineDiagnosticSignWarn = { bg = "surface", fg = "gold" },
+    --     StatusLineDiagnosticSignInfo = { bg = "surface", fg = "foam" },
+    --     StatusLineDiagnosticSignHint = { bg = "surface", fg = "iris" },
+    --     StatusLineDiagnosticSignOk = { bg = "surface", fg = "pine" },
+    --   },
+    -- },
   },
   {
     "craftzdog/solarized-osaka.nvim",
@@ -108,7 +117,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      -- colorscheme = "rose-pine",
     },
   },
 }
