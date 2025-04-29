9716ead92f0449a25e4ab0af7852f69c2503058a
xavier-castro <xavier_castro@icloud.com>
2025-04-29 16:41:27 -0500

    Add ghostty theme

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,26 +6,6 @@
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
 
--- tmux tmux-sessionizer
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
--- Navigate between windows using Ctrl + arrow keys
-vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
-vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
-vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
-vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
-
--- Resize window
-vim.keymap.set("n", "<C-left>", "<C-w><")
-vim.keymap.set("n", "<C-right>", "<C-w>>")
-vim.keymap.set("n", "<C-up>", "<C-w>+")
-vim.keymap.set("n", "<C-down>", "<C-w>-")
-
 -- Folding
 -- Nice and simple folding:
 vim.o.foldenable = true
