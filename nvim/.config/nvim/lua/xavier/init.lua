74b9a0ea77971c0d44631148255f5a7aa139875c
xavier-castro <xavier_castro@icloud.com>
2025-04-29 19:35:36 -0500

    Fix deprecation

--- nvim/.config/nvim/lua/xavier/init.lua
+++ nvim/.config/nvim/lua/xavier/init.lua
@@ -88,12 +88,8 @@ autocmd("LspAttach", {
 		vim.keymap.set("i", "<C-h>", function()
 			vim.lsp.buf.signature_help()
 		end, opts)
-		vim.keymap.set("n", "[d", function()
-			vim.diagnostic.goto_next()
-		end, opts)
-		vim.keymap.set("n", "]d", function()
-			vim.diagnostic.goto_prev()
-		end, opts)
+		vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+		vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 	end,
 })
 
