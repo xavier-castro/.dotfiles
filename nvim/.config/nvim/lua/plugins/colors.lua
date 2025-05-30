6db46ab6bbe53491a7611bcb3a6679b2c8859c84
xavier-castro <xavier_castro@icloud.com>
2025-05-30 08:20:42 -0500

    Setup

---
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("tokyonight").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+      })
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
+      })
+    end,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nightfox").setup({
+        options = {
+          transparent = vim.g.have_transparent_bg,
+        },
+      })
+      vim.cmd.colorscheme("nordfox")
+    end,
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,60 @@
+return { -- Autoformat (kept from original config)
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>F",
+      function()
+        require("conform").format({ async = true, lsp_format = "fallback" })
+      end,
+      mode = "",
+      desc = "[F]ormat buffer",
+    },
+    {
+      "<leader>lf",
+      function()
+        require("conform").format({ async = true, lsp_format = "fallback" })
+      end,
+      mode = "n",
+      desc = "[F]ormat buffer",
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 5000,
+          lsp_format = "fallback",
+        }
+      end
+    end,
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "black" },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      css = { "prettierd" },
+      html = { "prettierd" },
+      json = { "prettierd" },
+      yaml = { "prettierd" },
+      markdown = { "prettierd" },
+      graphql = { "prettierd" },
+    },
+    formatters = {
+      rubocop = {
+        command = "bundle",
+        prepend_args = { "exec", "rubocop" },
+        args = { "-A", "--stderr", "--stdin", "$FILENAME", "--format", "quiet" },
+        exit_codes = { 0, 1 },
+        timeout_ms = 10000,
+      },
+    },
+  },
+}
