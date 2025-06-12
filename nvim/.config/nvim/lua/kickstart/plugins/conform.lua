d0e867d682d4734beffb97b644bc56c1f209c756
xavier-castro <xavier_castro@icloud.com>
2025-06-12 16:57:47 -0500

    Add formatting

--- nvim/.config/nvim/lua/kickstart/plugins/conform.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/conform.lua
@@ -1,43 +1,61 @@
 return {
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>lf',
+      function()
+        require('conform').format { lsp_fallback = true, async = true, timeout_ms = 5000 }
       end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
+      desc = 'Format document',
     },
   },
+  opts = {
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      go = { 'goimports' }, -- "gofumpt"
+      javascript = { 'prettierd', 'prettier' },
+      typescript = { 'prettierd', 'eslint_d' },
+      javascriptreact = { 'prettierd', 'eslint_d' },
+      typescriptreact = { 'prettierd', 'eslint_d' },
+      astro = { 'prettierd', 'prettier' },
+      css = { 'prettierd', 'prettier' },
+      html = { 'prettierd', 'prettier' },
+      json = { 'prettierd' },
+      jsonc = { 'prettierd' },
+      yaml = { 'prettierd', 'prettier' },
+      markdown = { 'prettierd' },
+      graphql = { 'prettierd', 'prettier' },
+    },
+    default_format_opts = {
+      lsp_format = 'fallback',
+    },
+    format_after_save = function()
+      -- Stop if we disabled auto-formatting
+      if not vim.g.autoformat then
+        return nil
+      end
+
+      return {
+        async = true,
+        lsp_format = 'fallback',
+        timeout_ms = 500,
+      }
+    end,
+  },
+  init = function()
+    -- Use conform for gq.
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+    -- Start auto-formatting by default (and disable with my ToggleFormat command).
+    vim.g.autoformat = true
+  end,
+  config = function(_, opts)
+    local util = require 'conform.util'
+
+    -- running eslint fix
+    util.add_formatter_args(require 'conform.formatters.eslint_d', { '--fix' })
+    require('conform').setup(opts)
+  end,
 }
--- vim: ts=2 sts=2 sw=2 et
--- nvim/.config/nvim/lua/xavier/plugins/mini.lua
+++ nvim/.config/nvim/lua/xavier/plugins/mini.lua
@@ -24,11 +24,6 @@ return {
         require('mini.comment').setup()
         require('mini.completion').setup()
         require('mini.cursorword').setup()
-        require('mini.hues').setup()
-        local hues = require('mini.hues')
-        local fg = hues.get_colors().fg
-        local bg = hues.get_colors().bg
-        print("(fg: " .. fg .. ", bg: " .. bg .. ")")
         require('mini.indentscope').setup()
         require('mini.jump').setup()
         require('mini.jump2d').setup()
@@ -41,6 +36,145 @@ return {
         require('mini.surround').setup()
         require('mini.tabline').setup()
         require('mini.trailspace').setup()
+
+        -- Keymaps for MiniFiles (file explorer)
+        vim.keymap.set('n', '-', function()
+            require('mini.files').open()
+        end, {
+            desc = 'Open file explorer (MiniFiles)'
+        })
+
+        -- Sane keybinds for other Mini modules
+        vim.keymap.set('n', '<leader>bb', function()
+            require('mini.bufremove').delete()
+        end, {
+            desc = 'Delete buffer (MiniBufremove)'
+        })
+        vim.keymap.set('n', '<leader>ff', function()
+            require('mini.pick').builtin.files()
+        end, {
+            desc = 'Find files (MiniPick)'
+        })
+        vim.keymap.set('n', '<leader>ss', function()
+            require('mini.sessions').select()
+        end, {
+            desc = 'Select session (MiniSessions)'
+        })
+        vim.keymap.set('n', '<leader>qq', function()
+            require('mini.sessions').write()
+        end, {
+            desc = 'Save session (MiniSessions)'
+        })
+        vim.keymap.set('n', '<leader>/', function()
+            require('mini.fuzzy').fuzzy_search()
+        end, {
+            desc = 'Fuzzy search (MiniFuzzy)'
+        })
+        vim.keymap.set('n', '<leader>cc', function()
+            require('mini.comment').toggle_lines()
+        end, {
+            desc = 'Toggle comment (MiniComment)'
+        })
+        vim.keymap.set('n', '<leader>tt', function()
+            require('mini.trailspace').trim()
+        end, {
+            desc = 'Trim trailing whitespace (MiniTrailspace)'
+        })
+
+        -- Floating terminal toggle
+        local float_term_win = nil
+        local float_term_buf = nil
+        vim.keymap.set('n', '<leader>ft', function()
+            if float_term_win and vim.api.nvim_win_is_valid(float_term_win) then
+                vim.api.nvim_win_close(float_term_win, true)
+                float_term_win = nil
+                float_term_buf = nil
+            else
+                float_term_buf = vim.api.nvim_create_buf(false, true)
+                local width = math.floor(vim.o.columns * 0.8)
+                local height = math.floor(vim.o.lines * 0.8)
+                local row = math.floor((vim.o.lines - height) / 2)
+                local col = math.floor((vim.o.columns - width) / 2)
+                float_term_win = vim.api.nvim_open_win(float_term_buf, true, {
+                    relative = 'editor',
+                    width = width,
+                    height = height,
+                    row = row,
+                    col = col,
+                    style = 'minimal',
+                    border = 'rounded'
+                })
+                vim.fn.termopen(os.getenv('SHELL') or 'sh')
+                vim.cmd('startinsert')
+            end
+        end, {
+            desc = 'Toggle floating terminal'
+        })
+
+        -- Setup mini.clue for keybinding clues
+        require('mini.clue').setup({
+            triggers = { -- Leader triggers
+            {
+                mode = 'n',
+                keys = '<Leader>'
+            }, {
+                mode = 'x',
+                keys = '<Leader>'
+            }, -- File explorer
+            {
+                mode = 'n',
+                keys = '-'
+            }},
+            clues = { -- MiniFiles
+            {
+                mode = 'n',
+                keys = '-',
+                desc = 'Open file explorer (MiniFiles)'
+            }, -- MiniBufremove
+            {
+                mode = 'n',
+                keys = '<leader>bb',
+                desc = 'Delete buffer (MiniBufremove)'
+            }, -- MiniPick
+            {
+                mode = 'n',
+                keys = '<leader>ff',
+                desc = 'Find files (MiniPick)'
+            }, -- MiniSessions
+            {
+                mode = 'n',
+                keys = '<leader>ss',
+                desc = 'Select session (MiniSessions)'
+            }, {
+                mode = 'n',
+                keys = '<leader>qq',
+                desc = 'Save session (MiniSessions)'
+            }, -- MiniFuzzy
+            {
+                mode = 'n',
+                keys = '<leader>/',
+                desc = 'Fuzzy search (MiniFuzzy)'
+            }, -- MiniComment
+            {
+                mode = 'n',
+                keys = '<leader>cc',
+                desc = 'Toggle comment (MiniComment)'
+            }, -- MiniTrailspace
+            {
+                mode = 'n',
+                keys = '<leader>tt',
+                desc = 'Trim trailing whitespace (MiniTrailspace)'
+            }, -- Floating terminal
+            {
+                mode = 'n',
+                keys = '<leader>ft',
+                desc = 'Toggle floating terminal'
+            } -- Add more clues as needed for other Mini modules
+            },
+            window = {
+                delay = 200
+            }
+        })
 
         vim.cmd.colorscheme 'randomhue'
     end
