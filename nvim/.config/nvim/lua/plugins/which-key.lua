0dcef450f22ae01535d2884c186b9990539243be
xavier-castro <xavier_castro@icloud.com>
2025-07-06 02:41:57 -0500

    Yup

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++
@@ -1,9 +0,0 @@
--- Enhanced Which-Key configuration with Helix preset
-
----@type LazySpec
-return {
-  "folke/which-key.nvim",
-  opts = {
-    preset = "helix", -- Use Helix-style key bindings layout
-  },
-}
