0a599af293fe02d724d8057a8ff76406774178c5
xavier-castro <xavier_castro@icloud.com>
2025-07-08 18:20:47 -0500

    :_

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,47 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup("ThePrimeagen", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup("HighlightYank", {})
+
+function R(name)
+  require("plenary.reload").reload_module(name)
+end
+
+vim.filetype.add({
+  extension = {
+    templ = "templ",
+  },
+})
+
+autocmd("TextYankPost", {
+  group = yank_group,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 40,
+    })
+  end,
+})
+
+autocmd({ "BufWritePre" }, {
+  group = ThePrimeagenGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
+autocmd("BufEnter", {
+  group = ThePrimeagenGroup,
+  callback = function()
+    if vim.bo.filetype == "zig" then
+      vim.cmd.colorscheme("tokyonight-night")
+    else
+      vim.cmd.colorscheme("vague")
+    end
+  end,
+})
---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,5 @@
+return {
+  { "neovim/nvim-lspconfig", opts = {
+    inlay_hints = { enabled = false },
+  } },
+}
