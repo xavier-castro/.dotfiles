4516303c6cedf397b25ea487ef1435e9bb701175
xavier-castro <xavier_castro@icloud.com>
2025-07-11 16:48:20 -0500

    Add

--- nvim/.config/nvim/lua/theprimeagen/init.lua
+++ nvim/.config/nvim/lua/theprimeagen/init.lua
@@ -57,6 +57,20 @@ autocmd('BufEnter', {
     end
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function(args)
+    vim.lsp.start({
+      name = 'iwes',
+      cmd = {'iwes'},
+      root_dir = vim.fs.root(args.buf, {'.iwe' }),
+      flags = {
+        debounce_text_changes = 500
+      }
+    })
+  end,
+})
+
 
 autocmd('LspAttach', {
     group = ThePrimeagenGroup,
--- nvim/.config/nvim/lua/theprimeagen/lazy/lsp.lua
+++ nvim/.config/nvim/lua/theprimeagen/lazy/lsp.lua
@@ -109,6 +109,7 @@ return {
                 { name = "copilot", group_index = 2 },
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' }, -- For luasnip users.
+                { name = 'tags'},
             }, {
                 { name = 'buffer' },
             })
